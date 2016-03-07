5c5088a5ebf925cfe555f194456772e2b6ec883f
Fabio Quarantini <wlospam@gmail.com>
2016-03-07 10:10:15 +0100

    Updated code for disable emojis

--- inc/extend-backend.php
+++ inc/extend-backend.php
@@ -442,6 +442,7 @@ function ground_disable_emojis() {
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
 
 }
 
