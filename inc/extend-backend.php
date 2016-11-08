8d6cc13d7e9feffb49e37a75a1520d3e4e8d5132
Fabio Quarantini <wlospam@gmail.com>
2016-11-08 10:06:01 +0100

    Update disable emojis scripts

--- inc/extend-backend.php
+++ inc/extend-backend.php
@@ -445,6 +445,28 @@ function ground_disable_emojis() {
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+
+}
+
+function disable_emojis_tinymce( $plugins ) {
+
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+
+}
+
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+
+	if ( 'dns-prefetch' == $relation_type ) {
+		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+		$urls = array_diff( $urls, array( $emoji_svg_url ) );
+	}
+
+	return $urls;
 
 }
 
