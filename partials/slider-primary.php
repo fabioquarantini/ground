7b34cdf8939d6dec70beb6dd4f74913917c29ff8
Fabio Quarantini <wlospam@gmail.com>
2018-05-31 14:45:33 +0200

    Update owl carousel

--- partials/slider-primary.php
+++ partials/slider-primary.php
@@ -3,25 +3,25 @@
 	<div class="slider__item">
 		<img class="slider__img" src="http://via.placeholder.com/1920x1080">
 		<div class="slider__body">
-			<h3 class="slider__title">Title</h3>
+			<h3 class="slider__title">Title 1</h3>
 			<p class="slider__text">Text</p>
-			<a class="slider__btn button" href="#">More</a>
+			<a class="slider__button button" href="#">More</a>
 		</div>
 	</div>
 	<div class="slider__item">
 		<img class="slider__img" src="http://via.placeholder.com/1920x1080">
 		<div class="slider__body">
-			<h3 class="slider__title">Title</h3>
+			<h3 class="slider__title">Title 2</h3>
 			<p class="slider__text">Text</p>
-			<a class="slider__btn button" href="#">More</a>
+			<a class="slider__button button" href="#">More</a>
 		</div>
 	</div>
 	<div class="slider__item">
 		<img class="slider__img" src="http://via.placeholder.com/1920x1080">
 		<div class="slider__body">
-			<h3 class="slider__title">Title</h3>
+			<h3 class="slider__title">Title 3</h3>
 			<p class="slider__text">Text</p>
-			<a class="slider__btn button" href="#">More</a>
+			<a class="slider__button button" href="#">More</a>
 		</div>
 	</div>
 </div>
