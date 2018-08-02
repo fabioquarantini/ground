1078641532eb5dbc5b8d4658113df88194609a90
Fabio Quarantini <wlospam@gmail.com>
2018-08-02 12:28:03 +0200

    Add CDN scripts

--- inc/head-output.php
+++ inc/head-output.php
@@ -31,8 +31,15 @@ add_action( 'wp_enqueue_scripts', 'ground_enqueue_styles', 9 );
 function ground_enqueue_scripts() {
 
 	wp_deregister_script( 'jquery' );
-	wp_enqueue_script( 'jquery', "https://code.jquery.com/jquery-1.12.4.min.js", array(), null, true );
-
+	wp_enqueue_script( 'jquery', "https://code.jquery.com/jquery-3.3.1.min.js", array(), null, true );
+	wp_enqueue_script( 'imagesloaded', "https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/4.1.4/imagesloaded.pkgd.min.js", array(), null, true );
+	wp_enqueue_script( 'owlcarousel', "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js", array(), null, true );
+	wp_enqueue_script( 'fancybox', "https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js", array(), null, true );
+	wp_enqueue_script( 'infinitescroll', "https://cdnjs.cloudflare.com/ajax/libs/jquery-infinitescroll/3.0.4/infinite-scroll.pkgd.min.js", array(), null, true );
+	wp_enqueue_script( 'tweenmax', "https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js", array(), null, true );
+	wp_enqueue_script( 'scrollmagic', "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js", array(), null, true );
+	wp_enqueue_script( 'scrollmagic-gsap', "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.min.js", array(), null, true );
+	wp_enqueue_script( 'scrollmagic-indicators', "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js", array(), null, true );
 	wp_enqueue_script( 'scripts', TEMPLATE_URL . '/js/scripts.min.js', array( 'jquery' ), '1.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
