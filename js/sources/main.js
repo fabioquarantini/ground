d444b5f67a2444a76ace1a3f342bf00478cf8f5a
Fabio Quarantini <wlospam@gmail.com>
2018-08-08 16:27:03 +0200

    Add touch utility

--- js/sources/main.js
+++ js/sources/main.js
@@ -173,5 +173,21 @@ var siteInit = {
 
 		});
 
+	},
+
+	// Utility
+	utility: {
+
+		isTouch: function() {
+
+			if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+
+				return true;
+
+			}
+
+		}
+
 	}
+
 };
