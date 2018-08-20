9181cc9022b4cbc83d99a572328c47ba9e3fb697
Fabio Quarantini <wlospam@gmail.com>
2018-08-20 17:15:48 +0200

    Add no-image

--- partials/content-single-ground_catalog.php
+++ partials/content-single-ground_catalog.php
@@ -5,11 +5,14 @@
 	</header>
 
 	<div class="page__body">
-		<?php if ( has_post_thumbnail() ) { ?>
-			<figure class="media">
-				<?php the_post_thumbnail( 'medium', array( 'class' => 'media__img' ) ); ?>
-			</figure>
-		<?php } ?>
+		<figure class="media margin-bottom--1">
+			<?php if ( has_post_thumbnail() ) { ?>
+				<?php the_post_thumbnail( 'medium', array( 'class' => 'media__img full-width' ) ); ?>
+			<?php } else { ?>
+				<img class="media__img full-width" src="<?php echo TEMPLATE_URL ?>/img/no-image.svg">
+			<?php }?>
+		</figure>
+
 		<?php the_content(); ?>
 	</div> <!-- End .page__body -->
 
