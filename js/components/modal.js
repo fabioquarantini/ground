031c0314e62fa7c52b5278efc24ff2c0ee728e83
Daniel Duches <daniel.duches@evoluzionetelematica.it>
2019-01-10 09:25:42 +0100

    Slider e modal come moduli js

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,79 +1,27 @@
 require('@fancyapps/fancybox');
+import Debug from '../utilities/debug.js';
 
-$(document).ready(function() {
-	// TODO: Sistemare case insensitive
-	var $el = $('[href$=".jpg"], [href$=".png"], [href$=".gif"], [href$=".jpeg"], [href$=".webp"]');
-
-	if ($el.length == 0) {
-		return;
+export default class Modal {
+	constructor(classToAttach, options) {
+		//this.el = (classToAttach) ? $('[href$=".jpg"], [href$=".png"], [href$=".gif"], [href$=".jpeg"], [href$=".webp"], ' + classToAttach) : $('[href$=".jpg"], [href$=".png"], [href$=".gif"], [href$=".jpeg"], [href$=".webp"]');
+		this.el = (classToAttach) ? document.querySelectorAll(classToAttach) : document.querySelectorAll('[href$=".jpg"], [href$=".png"], [href$=".gif"], [href$=".jpeg"], [href$=".webp"]');
+		console.log(this.el);
+		this.options = options || {};
+		window.addEventListener('DOMContentLoaded', this.init());
+		window.addEventListener('NAVIGATE_IN', () => {
+			this.init();
+		});
+		window.addEventListener('NAVIGATE_OUT', () => {
+			//this.destroy();
+		});
 	}
 
-	$el.fancybox({
-		loop: true,
-		keyboard: true,
-		arrows: true,
-		infobar: true,
-		clickOutside: 'close',
-		transitionEffect: 'slide',
-		hash: false,
-		wheel: false,
-		buttons: [
-			//"zoom",
-			//"share",
-			//"slideShow",
-			//"fullScreen",
-			//"download",
-			//"thumbs",
-			'close'
-		],
-		clickContent: false,
-		clickSlide: false,
-		onActivate: function() {
-			$('html').addClass('overflow-hidden');
-		},
-		beforeClose: function() {
-			$('html').removeClass('overflow-hidden');
-		},
-		beforeShow: function(instance, slide) {
-			var $navigation = $('.fancybox-navigation');
-			var $navigationButtons = $navigation.find('.fancybox-button');
-			var $navigationButtonLeft = $navigation.find('.fancybox-button--arrow_left');
-			var $navigationButtonRight = $navigation.find('.fancybox-button--arrow_right');
-			var navigationHeight = $navigationButtons.height();
-			var $toolbar = $('.fancybox-toolbar');
-			var toolbarHeight = $toolbar.height();
-			var toolbarWidth = $toolbar.width();
-			var $container = $('.fancybox-container');
-			var containerHeight = $container.height();
-			var containerWidth = $container.width();
-			var scrollTop = $(window).scrollTop();
-
-			$container.addClass('modal--follow-mouse');
-			$navigationButtons.removeAttr('title');
-			$navigationButtons.addClass('is-active');
-
-			setTimeout(function() {
-				$navigationButtons.removeClass('is-active');
-			}, 400);
-
-			$container.on('mousemove', function(e) {
-				$navigationButtons.each(function() {
-					$(this).css({
-						left: e.pageX,
-						top: e.pageY - scrollTop
-					});
-				});
-
-				if (e.pageY - scrollTop < toolbarHeight && e.pageX > containerWidth - toolbarWidth) {
-					$navigationButtons.hide();
-				} else if (e.pageX < containerWidth / 2) {
-					$navigationButtonLeft.show();
-					$navigationButtonRight.hide();
-				} else {
-					$navigationButtonLeft.hide();
-					$navigationButtonRight.show();
-				}
-			});
+	init() {
+		if (this.el.length == 0) {
+			Debug.error('DOM node does not exist');
+			return;
 		}
-	});
-});
+
+		$(this.el).fancybox();
+	}
+}
--- js/components/slider.js
+++ js/components/slider.js
@@ -1,49 +1,53 @@
 import Swiper from 'swiper';
+import Debug from '../utilities/debug.js';
 
 export default class Slider {
-	constructor(classToAttach) {
+	constructor(classToAttach, options) {
+		this.el = classToAttach;
+		this.options = options || {};
 		window.addEventListener('DOMContentLoaded', this.init());
-		//TODO: fix highway event
 		window.addEventListener('NAVIGATE_IN', () => {
 			this.init();
+		});
+		window.addEventListener('NAVIGATE_OUT', () => {
+			this.destroy();
 		});
 	}
 
 	init() {
-		var $el = $('.js-slider-primary');
-
-		if ($el.length == 0) {
-			// TODO: log error?
+		if (document.querySelectorAll(this.el).length == 0) {
+			Debug.error('DOM node does not exist');
 			return;
 		}
 
-		var slider = new Swiper($el, {
-			//init: false,
-			direction: 'horizontal',
-			loop: true,
-			effect: 'slide',
-			autoHeight: false,
-			parallax: true,
-			preloadImages: false,
-			lazy: {
+		Debug.log('slider init');
+		this.slider = new Swiper(this.el, {
+			init: (this.options.init == false) ? false : true,
+			direction: this.options.direction || 'horizontal',
+			loop: (this.options.loop == false) ? false : true,
+			effect: this.options.effect || 'slide',
+			autoHeight: (this.options.autoHeight == false) ? false : true,
+			parallax: (this.options.parallax == false) ? false : true,
+			preloadImages: (this.options.preloadImages == false) ? false : false,
+			lazy: this.options.lazy || {
 				loadPrevNext: true,
 				loadPrevNextAmount: 1,
 				loadOnTransitionStart: true,
 			},
-			autoplay: {
+			autoplay: this.options.autoplay || {
 				delay: 3000
 			},
-			pagination: {
+			pagination: this.options.pagination || {
 				el: '.js-slider-primary-pagination',
 				clickable: true
 			},
-			navigation: {
+			navigation: this.options.navigation || {
 				prevEl: '.js-slider-primary-navigation-prev',
 				nextEl: '.js-slider-primary-navigation-next'
 			},
-			slidesPerView: 1,
-			spaceBetween: 0,
-			breakpoints: {
+			slidesPerView: this.options.slidesPerView || 1,
+			spaceBetween: this.options.spaceBetween || 0,
+			breakpoints: this.options.breakpoints || {
 				// when window width is <= 320px
 				320: {
 					slidesPerView: 1
@@ -55,18 +59,36 @@ export default class Slider {
 		});
 	}
 
+	start() {
+		if (this.slider === undefined) {
+			Debug.error('Slider does not exist');
+			return;
+		}
+		this.slider.init();
+	}
+
 	play() {
 		if (this.slider === undefined) {
+			Debug.error('Slider does not exist');
 			return;
 		}
 		this.slider.autoplay.start();
 	}
 
 	stop() {
+		if (this.slider === undefined) {
+			Debug.error('Slider does not exist');
+			return;
+		}
 		this.slider.autoplay.stop();
 	}
 
 	destroy() {
-		this.slider.destroy();
+		if (this.slider === undefined) {
+			Debug.error('Slider does not exist');
+			return;
+		} else {
+			this.slider.destroy(true, false);
+		}
 	}
 }
