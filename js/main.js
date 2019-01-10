9e979f5a4c86c960fabd9fc7fd4f2bd566f32489
Daniel Duches <daniel.duches@evoluzionetelematica.it>
2019-01-10 09:37:21 +0100

    Aggiunge import ai nuovi file 

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 import Utilities from './utilities/utilities.js';
+import './components/highway.js';
 import Slider from './components/slider.js';
+import Modal from './components/modal.js';
 
 //import './components/console.js';
-import './components/highway.js';
 //import './components/infiniteScroll.js';
 //import './components/loader.js';
-import './components/modal.js';
 //import './components/toggleClass.js';
 //import './components/lazyLoad.js';
 //import './components/animations.js';
 //import './components/parallax.js';
 
 import 'lazysizes';
-var slider = new Slider;
+var slider = new Slider('.js-slider-primary', {
+	init: false
+});
+slider.start();
+var modal = new Modal('.js-modal');
