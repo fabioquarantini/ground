8a95e44cc8282637e02e081786883c73235aa820
Fabio Quarantini <wlospam@gmail.com>
2019-03-06 15:43:54 +0100

    Raname main file in app.js

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ import InfiniteScroll from './components/infiniteScroll.js';
 import Modal from './components/modal.js';
 import Navigation from './components/navigation.js';
 import Slider from './components/slider.js';
+//import Parallax from './components/parallax.js';
 import Loader from './components/loader.js';
 import Split from './components/split.js';
+import ToggleClass from './components/toggleClass.js';
 
 const infiniteScroll = new InfiniteScroll;
 const loader = new Loader;
@@ -13,3 +15,5 @@ const navigation = new Navigation;
 const sliderPrimary = new Slider('.js-slider-primary');
 const sliderSecondary = new Slider('.js-slider-secondary');
 const split = new Split;
+const toggleClass = new ToggleClass;
+//const parallax = new Parallax('.js-parallax');
