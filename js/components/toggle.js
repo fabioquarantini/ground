450e773186654241ebfcb49f78008ca07cd7fb9e
Fabio Quarantini <wlospam@gmail.com>
2019-03-08 14:26:49 +0100

    Vanilla toggle module

---
+++ js/components/toggle.js
@@ -0,0 +1,56 @@
+/**
+ * Toggle module
+ */
+import * as deepmerge from 'deepmerge';
+
+export default class Toggle {
+	/**
+	 * @param {Object} options - User options
+	 */
+	constructor(options) {
+		this.defaults = {
+			element: '.js-toggle',
+			toggleClassName: 'is-active'
+		};
+		this.options = options ? deepmerge(this.defaults, options) : this.defaults;
+		this.element = document.querySelectorAll(this.options.element);
+
+		window.addEventListener('DOMContentLoaded', () => {
+			this.init();
+		});
+		window.addEventListener('NAVIGATE_IN', () => {
+			this.init();
+		});
+	}
+
+	init() {
+		if (this.element.length == 0) {
+			return;
+		}
+
+		this.element.forEach(element => {
+			element.addEventListener('click', (event) => {
+				event.preventDefault();
+
+				// Add data-toggle-class-name="customclass" to change the default class name
+				if (event.target.hasAttribute('data-toggle-class-name')) {
+					this.options.toggleClassName = event.target.dataset.toggleClassName;
+				}
+
+				// Add data-toggle-target=".selector1 #selector2" to toggle different target
+				if (event.target.hasAttribute('data-toggle-target')) {
+					let targetList = event.target.dataset.toggleTarget.split(' ');
+
+					targetList.forEach(element => {
+						let target = document.querySelectorAll(element);
+						target.forEach(element => {
+							element.classList.toggle(this.options.toggleClassName);
+						}, this);
+					}, this);
+				} else {
+					event.target.classList.toggle(this.options.toggleClassName);
+				}
+			});
+		}, this);
+	}
+}
