8df909a56168e1337d271a30e828477f6c46b6fd
Fabio Quarantini <wlospam@gmail.com>
2019-03-08 14:52:10 +0100

    Update split module

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import Slider from './components/slider.js';
 //import Parallax from './components/parallax.js';
 import Loader from './components/loader.js';
 import Split from './components/split.js';
-import ToggleClass from './components/toggleClass.js';
+import Toggle from './components/toggle.js';
 
 const infiniteScroll = new InfiniteScroll;
 const loader = new Loader;
@@ -14,6 +14,6 @@ const modal = new Modal('.js-modal');
 const navigation = new Navigation;
 const sliderPrimary = new Slider('.js-slider-primary');
 const sliderSecondary = new Slider('.js-slider-secondary');
-const split = new Split;
-const toggleClass = new ToggleClass;
+const split = new Split();
+const toggle = new Toggle();
 //const parallax = new Parallax('.js-parallax');
--- js/components/split.js
+++ js/components/split.js
@@ -1,17 +1,32 @@
+/**
+ * Split module
+ * Splitting text, grids, images and more!
+ * @see https://splitting.js.org
+ */
 import Splitting from 'splitting';
+import * as deepmerge from 'deepmerge';
 
 export default class Split {
-	constructor() {
-		window.addEventListener('DOMContentLoaded', this.init());
-		window.addEventListener('NAVIGATE_IN', () => {
+	/**
+	 * @param {Object} options - User options
+	 */
+	constructor(options) {
+		this.defaults = {
+			target: '[data-splitting]', // String selector, Element, Array of Elements, or NodeList
+			by: 'chars', // String of the plugin name
+			key: null //Optional String to prefix the CSS variables
+		};
+		this.options = options ? deepmerge(this.defaults, options) : this.defaults;
+
+		window.addEventListener('DOMContentLoaded', () => {
 			this.init();
 		});
-		window.addEventListener('NAVIGATE_OUT', () => {
-			//this.destroy();
+		window.addEventListener('NAVIGATE_IN', () => {
+			this.init();
 		});
 	}
 
 	init() {
-		Splitting();
+		Splitting(this.options);
 	}
 }
