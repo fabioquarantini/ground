2c38808b3d2562b19a7ef798ee92724ba161d15c
Fabio Quarantini <wlospam@gmail.com>
2019-03-14 16:26:44 +0100

    Update event dispatcher and doc

--- js/utilities/dispatcher.js
+++ js/utilities/dispatcher.js
@@ -1,11 +1,19 @@
-import Debug from './debug.js';
+/**
+ * Event dispatcher
+ */
+import { DEBUG_MODE } from '../utilities/environment';
 
 export default class Dispatcher {
-	static trigger(eventName, data) {
-		let event = new window.CustomEvent(eventName, {
-			detail: data
-		});
-		Debug.log('🚀 Triggered ' + eventName);
+	/**
+	 * Event trigger
+	 * @param {DOMString} eventName - Representing the name of the event.
+	 * @param {*} detail - Additional parameters to pass along to the event handler.
+	 */
+	static trigger(eventName, detail) {
+		const event = new window.CustomEvent(eventName, { detail });
 		window.dispatchEvent(event);
+		if (DEBUG_MODE) {
+			console.log('🚀 Triggered:', eventName);
+		}
 	}
 }
