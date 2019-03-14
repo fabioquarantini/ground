2c4374d09a0fd3074beda2b49230a8a38a6a323b
Fabio Quarantini <wlospam@gmail.com>
2019-03-14 14:45:58 +0100

    Update shared constants

--- js/utilities/environment.js
+++ js/utilities/environment.js
@@ -1,11 +1,5 @@
 /**
  * Shared constants
- * @todo Vanilla selectors
- * @todo $ Uppercase?
  */
 
-export const $document = $(document);
-export const $window = $(window);
-export const $html = $(document.documentElement);
-export const $body = $(document.body);
-export const DEBUG_MODE = true;
+export const DEBUG_MODE = false;
