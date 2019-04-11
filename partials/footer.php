72b49c13f4b9275c1445bc1afa76bcb7220a7869
Fabio Quarantini <wlospam@gmail.com>
2019-04-11 12:54:32 +0200

    Add cursor markup

--- partials/footer.php
+++ partials/footer.php
@@ -34,10 +34,12 @@
 		</footer> <!-- End .footer -->
 
 		<div class="cursor" id="js-cursor">
-			<div class="cursor__circle cursor__circle--outer"></div>
-			<div class="cursor__circle cursor__circle--inner">
-				<i class="cursor__icon"></i>
+			<div class="cursor__circle cursor__circle--outer" id="js-cursor-outer"></div>
+			<div class="cursor__circle cursor__circle--inner"></div>
+			<div class="centered">
+				<div class="cursor__loader"></div>
 			</div>
+			<i class="cursor__icon" id="js-cursor-icon"></i>
 		</div>
 			
 		<?php wp_footer();
