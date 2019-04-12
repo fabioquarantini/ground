01c6ebb625e94c6f05d69ae277a9db4580424206
Fabio Quarantini <wlospam@gmail.com>
2019-04-12 10:41:27 +0200

    Fix grid layout

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -24,55 +24,49 @@ get_template_part( 'partials/header' ); ?>
 
 <div class="container">
 
-	<div class="row margin-0">
+	<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
 
-		<div class="gr-12">
+	<div class="gr-12">
 
-			<section class="page page--woocommerce-single">
+		<section class="page page--woocommerce-single">
 
-				<header class="page__header">
-					<?php
-						/**
-						 * woocommerce_before_main_content hook.
-						 *
-						 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-						 * @hooked woocommerce_breadcrumb - 20
-						 */
-						do_action( 'woocommerce_before_main_content' );
-					?>
-				</header>
+			<div class="page__body">
 
-				<div class="page__body">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php while ( have_posts() ) : the_post(); ?>
+					<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
-						<?php wc_get_template_part( 'content', 'single-product' ); ?>
+				<?php endwhile; // end of the loop. ?>
 
-					<?php endwhile; // end of the loop. ?>
+				<?php
+					/**
+					 * woocommerce_after_main_content hook.
+					 *
+					 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+					 */
+					do_action( 'woocommerce_after_main_content' );
+				?>
 
-					<?php
-						/**
-						 * woocommerce_after_main_content hook.
-						 *
-						 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-						 */
-						do_action( 'woocommerce_after_main_content' );
-					?>
+			</div>
 
-				</div>
+		</section>
 
-			</section>
-
-			<?php
-				/**
-				 * woocommerce_sidebar hook.
-				 *
-				 * @hooked woocommerce_get_sidebar - 10
-				 */
-				do_action( 'woocommerce_sidebar' );
-			?>
-
-		</div>
+		<?php
+			/**
+			 * woocommerce_sidebar hook.
+			 *
+			 * @hooked woocommerce_get_sidebar - 10
+			 */
+			do_action( 'woocommerce_sidebar' );
+		?>
 
 	</div>
 
