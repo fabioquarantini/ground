3c8fb1f18f6208d2f131bb00bd9851f261248822
Fabio Quarantini <wlospam@gmail.com>
2019-04-12 10:42:03 +0200

    Update cursor module

--- js/components/cursor.js
+++ js/components/cursor.js
@@ -3,21 +3,18 @@
  * Mouse interactions
  */
 import * as deepmerge from 'deepmerge';
-import TweenMax from 'gsap/TweenMax';
 import Utilities from '../utilities/utilities';
-import {
-	log
-} from 'util';
+const isMobile = require('ismobilejs');
 
 export default class Cursor {
 	constructor(el) {
 		this.element = document.getElementById('js-cursor');
-		this.innerCursor = document.querySelector('.cursor__circle--inner');
-		this.outerCursor = document.querySelector('.cursor__circle--outer');
-		this.icon = document.querySelector('.cursor__icon');
-		this.bounds = {
-			dot: this.innerCursor.getBoundingClientRect(),
-			circle: this.outerCursor.getBoundingClientRect()
+		this.outerCursor = document.getElementById('js-cursor-outer');
+		this.icon = document.getElementById('js-cursor-icon');
+		this.bounds = this.element.getBoundingClientRect();
+		this.amount = {
+			inner: 0.15,
+			outer: 0.15
 		};
 		this.scale = 1;
 		this.opacity = 1;
@@ -26,14 +23,8 @@ export default class Cursor {
 			y: -100
 		};
 		this.lastMousePos = {
-			dot: {
-				x: 0,
-				y: 0
-			},
-			circle: {
-				x: 0,
-				y: 0
-			}
+			x: 0,
+			y: 0
 		};
 		this.lastScale = 1;
 		this.lastOpacity = 1;
@@ -42,88 +33,185 @@ export default class Cursor {
 			this.init();
 		});
 
+		window.addEventListener('NAVIGATE_IN', () => {
+			console.log('in');
+		});
+
+
 		window.addEventListener('NAVIGATE_END', () => {
+			console.log('end');
 			this.init();
+		});
+
+		window.addEventListener('fancyboxOnActivate', () => {
+			this.init();
+		});
+
+		window.addEventListener('infiniteScrollAppended', () => {
+			// Alla seconda chiamata vengono doppiati gli eventi
+			this.init();
+		});
+
+		window.addEventListener('NAVIGATE_OUT', () => {
+			this.destroyEvents();
+			console.log('out');
 		});
 	}
 
 	init() {
+		if (isMobile.any && document.querySelectorAll(this.element).length == 0) {
+			return;
+		}
+
 		this.initEvents();
+		//TODO: togliere il request animation frame alla chiusura  window.cancelAnimationFrame(this.requestId);
 		requestAnimationFrame(() => this.render());
+
+
+		/*let root = document;
+		var that = this;
+		function bindedUpdateNode(mutationsList, observer) {
+			for(var mutation of mutationsList) {
+				if (mutation.type == 'childList') {
+					if (mutation.addedNodes) {
+						root = mutation.addedNodes;
+						that.initEvents(root);
+					}
+				}
+				else if (mutation.type == 'attributes') {
+					console.log('The ' + mutation.attributeName + ' attribute was modified.', mutation);
+				}
+			}
+		}*/
+
+		function bindedUpdateNode(mutationsList, observer) {
+			for(var mutation of mutationsList) {
+				if (mutation.type == 'childList') {
+					if (mutation.addedNodes.length > 0) {
+						console.log('A child node has been added.');
+						//console.log(mutation);
+						console.log(mutation.addedNodes);
+					}
+				}
+				else if (mutation.type == 'attributes') {
+					console.log('The ' + mutation.attributeName + ' attribute was modified.');
+				}
+			}
+		}
+
+		this.domObserver = new window.MutationObserver(bindedUpdateNode);
+		this.domObserver.observe(document.body, {
+			childList: true,
+			attributes: false,
+			characterData: false,
+			subtree: true
+		});
 	}
 
 	initEvents() {
-		window.addEventListener('mousemove', event => this.mousePos = Utilities.getMousePosition(event));
+		window.addEventListener('mousemove', event => this.mousePosition(event));
 
 		// TODO: non aggiorna i nuovi selettori ajax
-		// Custom cursor chnages state when hovering on elements with 'data-hover'.
-		[...document.querySelectorAll('a, .slider__navigation, .button, [data-fancybox="gallery"]')].forEach((link) => {
-			//[...document.querySelectorAll('[data-hover]')].forEach((link) => {
-			link.addEventListener('mouseenter', (event) => this.enter(event));
-			link.addEventListener('mouseleave', (event) => this.leave(event));
-			link.addEventListener('click', (event) => this.click(event));
+		[
+			...document.querySelectorAll('a, .slider__navigation, .button, [data-fancybox="gallery"], .fancybox-button')
+		].forEach(link => {
+			//console.log("links", link);
+			link.addEventListener('mouseenter', event => this.enter(event));
+			link.addEventListener('mouseleave', event => this.leave(event));
+			link.addEventListener('click', event => this.click(event));
 		});
 	}
 
 	render() {
-		this.lastMousePos.dot.x = Utilities.lerp(this.lastMousePos.dot.x, this.mousePos.x - this.bounds.dot.width / 2, 1);
-		this.lastMousePos.dot.y = Utilities.lerp(this.lastMousePos.dot.y, this.mousePos.y - this.bounds.dot.height / 2, 1);
-		this.lastMousePos.circle.x = Utilities.lerp(this.lastMousePos.circle.x, this.mousePos.x - this.bounds.circle.width / 2, 0.15);
-		this.lastMousePos.circle.y = Utilities.lerp(this.lastMousePos.circle.y, this.mousePos.y - this.bounds.circle.height / 2, 0.15);
+		this.lastMousePos.x = Utilities.lerp(
+			this.lastMousePos.x,
+			this.mousePos.x - this.bounds.width / 2,
+			this.amount.outer
+		);
+		this.lastMousePos.y = Utilities.lerp(
+			this.lastMousePos.y,
+			this.mousePos.y - this.bounds.height / 2,
+			this.amount.outer
+		);
 		this.lastScale = Utilities.lerp(this.lastScale, this.scale, 0.15);
 		this.lastOpacity = Utilities.lerp(this.lastOpacity, this.opacity, 0.1);
-		this.innerCursor.style.transform = `translateX(${(this.lastMousePos.dot.x)}px) translateY(${this.lastMousePos.dot.y}px)`;
-		this.outerCursor.style.transform = `translateX(${(this.lastMousePos.circle.x)}px) translateY(${this.lastMousePos.circle.y}px) scale(${this.lastScale})`;
+		this.element.style.transform = `translateX(${this.lastMousePos.x}px) translateY(${this.lastMousePos.y}px)`;
+		this.outerCursor.style.transform = `scale(${this.lastScale})`;
 		this.outerCursor.style.opacity = this.lastOpacity;
 		requestAnimationFrame(() => this.render());
 	}
 
+	mousePosition(event) {
+		this.mousePos = Utilities.getMousePosition(event);
+	}
+
 	enter(event) {
-		this.element.classList.toggle('is-enter');
 		this.scale = 2;
-
-		if (event.target.classList.contains('slider__navigation')) {
-			this.scale = 1.5;
-			this.element.classList.toggle('is-navigation');
-		}
-
-		if (event.target.classList.contains('slider__navigation--next')) {
-			this.icon.classList.toggle('icon-arrow-right');
-		}
-
-		if (event.target.classList.contains('slider__navigation--prev')) {
-			this.icon.classList.toggle('icon-arrow-left');
-		}
-
-		if (event.target.hasAttribute('data-fancybox')) {
-			this.scale = 1.5;
-			this.element.classList.toggle('is-zoom');
-			this.icon.classList.toggle('icon-plus');
-		}
+		this.toggle(event);
 	}
 
 	leave(event) {
-		this.element.classList.toggle('is-enter');
 		this.scale = 1;
-		if (event.target.classList.contains('slider__navigation')) {
-			this.element.classList.toggle('is-navigation');
+		this.toggle(event);
+	}
+
+	toggle(event) {
+		this.element.classList.toggle('is-cursor-hover');
+
+		if (
+			event.target.classList.contains('slider__navigation') ||
+			event.target.classList.contains('fancybox-button')
+		) {
+			this.element.classList.toggle('is-cursor-navigation');
 		}
-		if (event.target.classList.contains('slider__navigation--next')) {
+
+		if (
+			event.target.classList.contains('slider__navigation--next') ||
+			event.target.classList.contains('fancybox-button--arrow_right')
+		) {
 			this.icon.classList.toggle('icon-arrow-right');
 		}
 
-		if (event.target.classList.contains('slider__navigation--prev')) {
+		if (
+			event.target.classList.contains('slider__navigation--prev') ||
+			event.target.classList.contains('fancybox-button--arrow_left')
+		) {
 			this.icon.classList.toggle('icon-arrow-left');
 		}
 
+		if (event.target.classList.contains('fancybox-button--close')) {
+			this.icon.classList.toggle('icon-close');
+		}
+
 		if (event.target.hasAttribute('data-fancybox')) {
-			this.element.classList.toggle('is-zoom');
+			this.element.classList.toggle('is-cursor-zoom');
 			this.icon.classList.toggle('icon-plus');
+		}
+
+		if (event.target.classList.contains('js-cursor-hide')) {
+			this.element.classList.toggle('is-cursor-hide');
 		}
 	}
 
 	click(event) {
 		this.lastScale = 1;
 		this.lastOpacity = 0;
+	}
+
+	destroy() {}
+
+	destroyEvents() {
+		//this.domObserver.disconnect();
+
+		// TODO detroy events
+		window.removeEventListener('mousemove', event => this.mousePosition(event));
+
+		[
+			...document.querySelectorAll('a, .slider__navigation, .button, [data-fancybox="gallery"], .fancybox-button')
+		].forEach(link => {
+			link.removeEventListener('mouseenter', event => this.enter(event));
+			link.removeEventListener('mouseleave', event => this.leave(event));
+			link.removeEventListener('click', event => this.click(event));
+		});
 	}
 }
