e306f068ba38b51e91d95cfd779fddf11255c258
Fabio Quarantini <wlospam@gmail.com>
2019-04-15 14:27:40 +0200

    Update post footer

--- partials/abstract-post.php
+++ partials/abstract-post.php
@@ -19,13 +19,13 @@
 	<p class="item__body"><?php ground_excerpt( 100 ); ?></p>
 
 	<footer class="item__footer">
-		<span class="item__comments"><?php if( comments_open() && !post_password_required() ) {
-			comments_popup_link( __('No comments yet', 'ground'), __('1 comment', 'ground'), __('% comments', 'ground'), __('comments-link', 'ground'), __('Comments are off for this post','ground') );
-		} ?></span> -
-		<time class="item__date" datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time> -
-		<span class="item__author"><?php the_author_posts_link(); ?></span> -
-		<span class="item__category"><?php the_category(', '); ?></span>
-		<?php if ( has_tag() ) { ?> - <?php the_tags('<span class="item__tag">', ',', '</span>' ); ?><?php } ?>
+		<span class="item__data item__data--date"><?php _e('Date', 'ground') ?>: <time datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time></span>
+		<span class="item__data item__data--author"><?php _e('Author', 'ground') ?>: <?php the_author_posts_link(); ?></span>
+		<span class="item__data item__data--category"><?php _e('Category', 'ground') ?>: <?php the_category(', '); ?></span>
+		<?php if ( has_tag() ) { ?> <?php the_tags('<span class="item__data item__data--tag">' . __('Tag', 'ground') . ': ', ', ', '</span>' ); ?><?php } ?>
+		<?php if( comments_open() && !post_password_required() ) { ?>
+			<span class="item__data item__data--comments"><?php _e('Comments', 'ground') ?>: <?php comments_popup_link(); ?></span>
+		<?php } ?>
 	</footer> <!-- End .item__footer -->
 
 </article> <!-- End .item -->
--- partials/content-single-post.php
+++ partials/content-single-post.php
@@ -14,14 +14,14 @@
 	</div> <!-- End .page__body -->
 
 	<footer class="page__footer">
+		<span class="page__data page__data--date"><?php _e('Date', 'ground') ?>: <time datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time></span>
+		<span class="page__data page__data--author"><?php _e('Author', 'ground') ?>: <?php the_author_posts_link(); ?></span>
+		<span class="page__data page__data--category"><?php _e('Category', 'ground') ?>: <?php the_category(', '); ?></span>
+		<?php if ( has_tag() ) { ?> <?php the_tags('<span class="page__data page__data--tag">' . __('Tag', 'ground') . ': ', ', ', '</span>' ); ?><?php } ?>
 		<?php if( comments_open() && !post_password_required() ) { ?>
-			<span class="page__comments"><?php comments_popup_link( __('No comments yet', 'ground'), __('1 comment', 'ground'), __('% comments', 'ground'), __('comments-link', 'ground'), __('Comments are off for this post','ground') ); ?></span> -
+			<span class="page__data page__data--comments"><?php _e('Comments', 'ground') ?>: <?php comments_popup_link(); ?></span>
 		<?php } ?>
-		<time class="page__date" datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time> -
-		<span class="page__author"><?php the_author_posts_link(); ?></span> -
-		<span class="page__category"><?php the_category(', '); ?></span>
-		<?php if ( has_tag() ) { ?> - <?php the_tags('<span class="page__tag">', ',', '</span>' ); ?><?php } ?>
-	</footer>
+	</footer> <!-- End .page__footer -->
 
 	<?php if ( comments_open() || get_comments_number() ) {
 		comments_template('/partials/comments.php');
