cc4ddd1a53ec2905106f933f3b475f745cdd70a3
Fabio Quarantini <wlospam@gmail.com>
2019-06-18 14:29:50 +0200

    Fix file_get_contents

--- partials/header.php
+++ partials/header.php
@@ -18,10 +18,14 @@
 
 			<a class="logo js-cursor-hide" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo('name'); ?>">
 				<?php /* <img class="logo__img" src="<?php echo TEMPLATE_URL ?>/img/logo.svg" alt="<?php bloginfo('name'); ?>" /> */ ?>
-				<?php echo file_get_contents(TEMPLATE_URL ."/img/logo.svg"); ?>
+				<?php echo file_get_contents(TEMPLATE_PATH ."/img/logo.svg"); ?>
 			</a> <!-- End .logo -->
 
 			<?php get_template_part( 'partials/navigation', 'primary' ); ?>
+
+			<button class="header__search js-toggle" data-toggle-target=".search" data-toggle-class-name="is-search-open">
+				<i class="icon-magnifying-glass"></i>
+			</button> <!-- End .navicon -->
 
 			<?php get_template_part( 'partials/woocommerce/minicart' ); ?>
 
