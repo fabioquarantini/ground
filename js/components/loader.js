9b7d105f65fd33c6739b60065cd200853342dc2e
Fabio Quarantini <wlospam@gmail.com>
2019-08-07 16:53:23 +0200

    Update selector

--- js/components/loader.js
+++ js/components/loader.js
@@ -3,42 +3,42 @@ import TweenMax from 'gsap/TweenMax';
 
 export default class Loader {
 	constructor() {
-		this.element = document.getElementById('js-loader');
-		this.elementBg = document.getElementById('js-loader-bg');
-		this.elementContent = document.getElementById('js-loader-content');
+		this.DOM = { element: document.getElementById('js-loader')};
+		this.DOM.body = document.body;
+		this.DOM.background = document.getElementById('js-loader-bg');
+		this.DOM.content = document.getElementById('js-loader-content');
 		this.tlLoader = new TimelineLite();
 		this.tlLoaderContent = new TimelineLite({
 			delay: 0.2
 		});
 
-		new imagesLoaded(document.body, { background: true }, this.init());
+		new imagesLoaded(this.DOM.body, { background: true }, this.init());
 	}
 
 	init() {
-		if (this.element.length == 0) {
+		if (!this.DOM.element) {
 			return;
 		}
 		// Reset Scroll
 		window.scrollTo(0, 0);
 
 		// Update body class
-		document.body.classList.replace('is-loading', 'is-loaded');
-
+		this.DOM.body.classList.replace('is-loading', 'is-loaded');
 
 		// Animations
-		this.tlLoader.to(this.elementBg, 1.5, {
+		this.tlLoader.to(this.DOM.background, 1.5, {
 			yPercent: 100,
 			force3D: true,
 			rotation: 0.01,
 			ease: Quart.easeInOut,
 			onComplete: () => {
 				// Update body class
-				document.body.classList.add('is-loader-complete');
+				this.DOM.body.classList.add('is-loader-complete');
 				// Hide loader
-				this.element.classList.add('display-none');
+				this.DOM.element.classList.add('display-none');
 			}
 		});
-		this.tlLoaderContent.to(this.elementContent, 0.8, {
+		this.tlLoaderContent.to(this.DOM.content, 0.8, {
 			ease: Power3.easeOut,
 			y: 65,
 			opacity: 0
