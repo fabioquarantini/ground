4a4444483bd39ae0728723bdee0b38a7e962bb41
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2019-08-21 17:45:25 +0200

    magnet.js from jquery to javascript

--- js/components/magnet.js
+++ js/components/magnet.js
@@ -6,72 +6,62 @@ export default class Magnet {
 	 * @param {Object} options - User options
 	 */
 	constructor(options) {
-
-        this.DOM = document.querySelectorAll('.js-magnet');
-
-		new imagesLoaded(document.body, { background: true }, this.init());
-
-		window.addEventListener('DOMContentLoaded', () => {
-			this.init();
-		});
-
-		window.addEventListener('NAVIGATE_IN', () => {
-			this.init();
-		});
-
-		window.addEventListener('infiniteScrollAppended', () => {
-			this.init();
-		});
+        this.DOM = { element: document.querySelectorAll('.js-magnet')};
+		this.DOM.body = document.body;
+        
+		new imagesLoaded(this.DOM.body, { background: true }, this.init());
 	}
 
 	init() {
 
-        if (this.DOM.length == 0) {
+        if (this.DOM.element.length == 0) {
 			return;
 		}
 
-        this.DOM.forEach(function(elem){
-            $(document).on('mousemove touch', function(e){
-                magnetize(elem, e);
-            });
-        })
+        this.DOM.element.forEach(element => {
 
-        // $(document).on('mousemove touch', function(e){
-        //   magnetize('.el', e);
-        // });
+            window.addEventListener('mousemove', event => this.magnetize(element, event));
 
-        function magnetize(el, e){
-            let mX = e.pageX,
-                mY = e.pageY;
-            const item = $(el);
+        });
+
+    }
+    
+
+    /**
+     * Magnetize
+    */
+    magnetize(element, event) {
+
+        let mX = event.pageX,
+            mY = event.pageY;
+        const item = element;
+        
+        const customDist = item.getAttribute('data-dist') * 20 || 120;
+        const centerX = item.offsetLeft + (item.clientWidth/2);
+        const centerY = item.offsetTop + (item.clientHeight/2);
+        
+        let deltaX = Math.floor((centerX - mX)) * -0.45;
+        let deltaY = Math.floor((centerY - mY)) * -0.45;
+        
+        let distance = this.calculateDistance(item, mX, mY);
             
-            const customDist = item.data('dist') * 20 || 120;
-            const centerX = item.offset().left + (item.width()/2);
-            const centerY = item.offset().top + (item.height()/2);
-            
-            let deltaX = Math.floor((centerX - mX)) * -0.45;
-            let deltaY = Math.floor((centerY - mY)) * -0.45;
-            
-            let distance = calculateDistance(item, mX, mY);
-                
-            if(distance < customDist){
-                TweenMax.to(item, 0.5, { y: deltaY, x: deltaX, scale: 1.2 });
-                item.addClass('is-magnet');
-            }
-            else {
-                TweenMax.to(item, 0.6, { y: 0, x: 0, scale: 1, });
-                item.removeClass('is-magnet');
-            }
+        if(distance < customDist) {
+            TweenMax.to(item, 0.5, { y: deltaY, x: deltaX, scale: 1.2 });
+            item.classList.add('is-magnet');
+        } else {
+            TweenMax.to(item, 0.6, { y: 0, x: 0, scale: 1, });
+            item.classList.remove('is-magnet');
         }
 
-        function calculateDistance(elem, mouseX, mouseY) {
-            return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
-        }
+    }
 
-        /*- MOUSE STICKY -*/
-        function lerp(a, b, n) {
-            return (1 - n) * a + n * b
-        }
+    /**
+     * calculateDistance
+    */
+    calculateDistance(element, mouseX, mouseY) {
 
-	}
+        return Math.floor(Math.sqrt(Math.pow(mouseX - (element.offsetLeft+(element.clientWidth/2)), 2) + Math.pow(mouseY - (element.offsetTop+(element.clientHeight/2)), 2)));
+    
+    }
+
 }
