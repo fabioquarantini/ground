919ae8c8ceed15dee06250df2f29adeb5c127b38
Fabio Quarantini <wlospam@gmail.com>
2020-02-19 17:16:28 +0100

    Add cash on delivery fee

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -26,6 +26,7 @@
 	22 - Remove woocommerce_catalog_ordering
 	23 - Change number of products that are displayed per page (shop page)
 	24 - Change Text catalog_orderby Button
+	25 - Cash on delivery fee
 
 ==========================================================================  */
 
@@ -575,3 +576,36 @@ function ground_woocommerce_customize_product_sorting($sorting_options){
 }
 
 //add_filter('woocommerce_catalog_orderby', 'ground_woocommerce_customize_product_sorting');
+
+/*  ==========================================================================
+	25 - Cash on delivery fee
+	==========================================================================  */
+
+// Add a custom fee based on cash on delivery
+function ground_woocommerce_cash_on_delivery_fee ( $cart ) {
+	if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
+		return;
+	}
+
+	if ( 'cod' === WC()->session->get('chosen_payment_method') ) {
+		$fee = 5;
+		$cart->add_fee( 'Handling', $fee, false );
+	}
+}
+
+// add_action( 'woocommerce_cart_calculate_fees', 'ground_woocommerce_cash_on_delivery_fee', 10, 1 );
+
+// Update checkout on method payment change
+function ground_woocommerce_cash_on_delivery_fee_js_update() {
+	if ( is_checkout() && ! is_wc_endpoint_url() ) { ?>
+		<script type="text/javascript">
+		jQuery( function($){
+			$('form.checkout').on('change', 'input[name="payment_method"]', function(){
+				$(document.body).trigger('update_checkout');
+			});
+		});
+		</script>
+	<?php }
+}
+
+// add_action( 'wp_footer', 'ground_woocommerce_cash_on_delivery_fee_js_update' );
