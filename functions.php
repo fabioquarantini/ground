e35593a6cece7316a4d7ec09f15cedac7ed09b96
Fabio Quarantini <wlospam@gmail.com>
2020-09-25 17:17:15 +0200

    Fix WordPress coding standards

--- functions.php
+++ functions.php
@@ -16,18 +16,18 @@ require_once 'inc/head-output.php';
 require_once 'inc/navigations.php';
 require_once 'inc/shortcode.php';
 
-if (class_exists( 'WooCommerce' )) {
-	require_once('inc/woocommerce/breadcrumbs.php');
-	require_once('inc/woocommerce/cart.php');
-	require_once('inc/woocommerce/category.php');
-	require_once('inc/woocommerce/checkout.php');
-	require_once('inc/woocommerce/pagination.php');
-	require_once('inc/woocommerce/payments.php');
-	require_once('inc/woocommerce/price.php');
-	require_once('inc/woocommerce/product-loop.php');
-	require_once('inc/woocommerce/product-single.php');
-	require_once('inc/woocommerce/rest.php');
-	require_once('inc/woocommerce/sidebar.php');
-	require_once('inc/woocommerce/support.php');
-	require_once('inc/woocommerce/tabs.php');
+if ( class_exists( 'WooCommerce' ) ) {
+	require_once 'inc/woocommerce/breadcrumbs.php';
+	require_once 'inc/woocommerce/cart.php';
+	require_once 'inc/woocommerce/category.php';
+	require_once 'inc/woocommerce/checkout.php';
+	require_once 'inc/woocommerce/pagination.php';
+	require_once 'inc/woocommerce/payments.php';
+	require_once 'inc/woocommerce/price.php';
+	require_once 'inc/woocommerce/product-loop.php';
+	require_once 'inc/woocommerce/product-single.php';
+	require_once 'inc/woocommerce/rest.php';
+	require_once 'inc/woocommerce/sidebar.php';
+	require_once 'inc/woocommerce/support.php';
+	require_once 'inc/woocommerce/tabs.php';
 }
