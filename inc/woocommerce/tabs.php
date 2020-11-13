b88da774bf4a764d61614764e0bcfdf2082ce4ca
Fabio Quarantini <wlospam@gmail.com>
2020-11-13 11:06:13 +0100

    Update tabs documentation

--- inc/woocommerce/tabs.php
+++ inc/woocommerce/tabs.php
@@ -1,15 +1,11 @@
 <?php
+/**
+ * Tabs
+ *
+ * @package Ground
+ */
 
-
-/*  == TABS ===============================================================
-
-    1 - Remove woocommerce_product_tabs in single-product
-
-==========================================================================  */
-
-
-/*  ==========================================================================
-	1 - Remove woocommerce_product_tabs in single-product
-	==========================================================================  */
-
-// remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+/**
+ * Remove single product tabs
+ */
+//remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
