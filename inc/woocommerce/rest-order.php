f8ca9329ff68e3eede6f411a2b6ccc45d13b92d4
Fabio Quarantini <wlospam@gmail.com>
2020-11-13 10:49:12 +0100

    Remove line items

--- inc/woocommerce/rest-order.php
+++ inc/woocommerce/rest-order.php
@@ -23,13 +23,6 @@ function ground_woocommerce_rest_prepare_shop_order( $response, $object, $reques
 	$order_data['billing']['pec']           = get_post_meta( $object->get_id(), '_billing_pec', true );
 	$order_data['billing']['sdi']           = get_post_meta( $object->get_id(), '_billing_sdi', true );
 
-	foreach ( $order_data['line_items'] as $key => $item ) {
-
-		$order_data['line_items'][ $key ]['price_regular_original'] = wc_get_order_item_meta( $item['id'], 'price_regular_original', true );
-		$order_data['line_items'][ $key ]['price_sale_original']    = wc_get_order_item_meta( $item['id'], 'price_sale_original', true );
-		$order_data['line_items'][ $key ]['type']                   = wc_get_order_item_meta( $item['id'], 'type', true );
-	}
-
 	$response->data = $order_data;
 	return $response;
 }
