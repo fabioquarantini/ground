ad4fdb1e2947f3d086fff29511c896d0bf4d8dfd
Fabio Quarantini <wlospam@gmail.com>
2020-11-13 11:16:34 +0100

    Update doc

--- inc/woocommerce/product-single.php
+++ inc/woocommerce/product-single.php
@@ -1,29 +1,21 @@
 <?php
+/**
+ * Product single
+ *
+ * @package Ground
+ */
 
-/*  == PRODUCT SINGLE ========================================================
+/**
+ * Add brand in product single
+ */
+function ground_woocommerce_add_brand_single_product() {
+	global $post;
+	echo '<h2 class="woocommerce-product-details__brand">' . get_the_term_list($post->ID, 'pa_brand', '', ', ') . '</h2>';
+}
 
-        1 - Add brand in product single
-        2 - Remove woocommerce_product_tabs in single-product
+// add_action('woocommerce_single_product_summary', 'ground_woocommerce_add_brand_single_product', 6);
 
-    ==========================================================================  */
-
-
-
-/*  ==========================================================================
-	1 - Add brand in product single
-	==========================================================================  */
-
-    function ground_woocommerce_add_brand_single_product() {
-        global $post;
-        echo '<h2 class="woocommerce-product-details__brand">' . get_the_term_list($post->ID, 'pa_brand', '', ', ') . '</h2>';
-    }
-    
-    // add_action('woocommerce_single_product_summary', 'ground_woocommerce_add_brand_single_product', 6);
-    
-
-
-/*  ==========================================================================
-	2 - Remove woocommerce_product_tabs in single-product
-	==========================================================================  */
-
-    // remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+/**
+ * Remove single product tabs
+ */
+// remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
