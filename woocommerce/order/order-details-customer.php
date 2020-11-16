d0cb3e57ac44aa9dc8e85eaf875887414978acda
Fabio Quarantini <wlospam@gmail.com>
2020-11-16 09:16:10 +0100

    Update order details fields

--- woocommerce/order/order-details-customer.php
+++ woocommerce/order/order-details-customer.php
@@ -17,10 +17,44 @@
 
 defined( 'ABSPATH' ) || exit;
 
+$show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_address();
 ?>
 <section class="woocommerce-customer-details">
 
+	<?php if ( $show_shipping ) : ?>
+
 	<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
+		<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
+
+	<?php endif; ?>
+
+	<h2 class="woocommerce-column__title"><?php esc_html_e( 'Dati personali', 'woocommerce' ); ?></h2>
+
+	<address>
+		<?php //echo wp_kses_post( $order->get_formatted_billing_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
+
+		<p class="woocommerce-customer-details--name margin-bottom-0">
+		<?php if ( $order->get_billing_first_name() ) : ?>
+			<?php echo esc_html( $order->get_billing_first_name() ); ?>
+		<?php endif; ?>
+
+		<?php if ( $order->get_billing_last_name() ) : ?>
+			<?php echo esc_html( $order->get_billing_last_name() ); ?>
+		<?php endif; ?>
+		</p>
+
+		<?php if ( $order->get_billing_phone() ) : ?>
+			<p class="woocommerce-customer-details--phone margin-bottom-0"><?php echo esc_html( $order->get_billing_phone() ); ?></p>
+		<?php endif; ?>
+
+		<?php if ( $order->get_billing_email() ) : ?>
+			<p class="woocommerce-customer-details--email"><?php echo esc_html( $order->get_billing_email() ); ?></p>
+		<?php endif; ?>
+	</address>
+
+	<?php if ( $show_shipping ) : ?>
+
+		</div><!-- /.col-1 -->
 
 		<div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
 			<h2 class="woocommerce-column__title"><?php esc_html_e( 'Shipping address', 'woocommerce' ); ?></h2>
@@ -30,6 +64,8 @@ defined( 'ABSPATH' ) || exit;
 		</div><!-- /.col-2 -->
 
 	</section><!-- /.col2-set -->
+
+	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
 
