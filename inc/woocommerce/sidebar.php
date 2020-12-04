7514259a9d1f82f2c14fbb3cf5316347abb3f16f
Fabio Quarantini <wlospam@gmail.com>
2020-12-04 12:59:38 +0100

    Update sidebar doc

--- inc/woocommerce/sidebar.php
+++ inc/woocommerce/sidebar.php
@@ -1,38 +1,37 @@
 <?php
+/**
+ * Sidebar
+ *
+ * @package Ground
+ */
 
-/*  == SIDEBAR ==================================================================
+/**
+ * Disable default WooCommerce sidebar
+ *
+ * @return void
+ */
+function ground_woocommerce_disable_woocommerce_sidebar() {
+	remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
+}
 
-        1 - Disable WooCommerce sidebar
-        2 - WooCommerce Register Sidebar
+add_action( 'init', 'ground_woocommerce_disable_woocommerce_sidebar' );
 
-    ==========================================================================  */
+/**
+ * Register custom WooCommerce sidebar
+ *
+ * @return void
+ */
+function ground_woocommerce_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => __( 'Shop sidebar', 'ground' ),
+			'id'            => 'sidebar-woocommerce',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h6 class="widgettitle">',
+			'after_title'   => '</h6>',
+		)
+	);
+}
 
-
-/*  ==========================================================================
-	1 - Disable WooCommerce sidebar
-	==========================================================================  */
-
-    function ground_disable_woocommerce_sidebar() {
-        remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
-    }
-    
-    add_action('init', 'ground_disable_woocommerce_sidebar');
-    
-
-    
-/*  ==========================================================================
-	2 - WooCommerce Register Sidebar
-	==========================================================================  */
-
-    function ground_woocommerce_widgets_init() {
-        register_sidebar( array(
-            'name' => __( 'WooCommerce', 'ground' ),
-            'id' => 'sidebar-woocommerce',
-            'before_widget' => '<div id="%1$s" class="widget %2$s">',
-            'after_widget' => '</div>',
-            'before_title' => '<h6 class="widgettitle">',
-            'after_title' => '</h6>',
-        ) );
-    }
-    
-    add_action( 'widgets_init', 'ground_woocommerce_widgets_init' );
+add_action( 'widgets_init', 'ground_woocommerce_widgets_init' );
