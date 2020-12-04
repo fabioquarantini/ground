76f90d898ded4c14e3af7a8ddfc4fd74d52855d1
Fabio Quarantini <wlospam@gmail.com>
2020-12-04 12:24:44 +0100

    Update product loop doc

--- inc/woocommerce/product-loop.php
+++ inc/woocommerce/product-loop.php
@@ -1,70 +1,72 @@
 <?php
+/**
+ * Products loop
+ *
+ * @package Ground
+ */
 
-/*  == PRODUCT LOOP ===========================================================
+/**
+ *  Remove add to cart button in product loop
+ */
+// remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
 
-        1 - Remove loop add to cart button
-        2 - Add term attribute in product loop
-        3 - Change Text catalog_orderby Button
-        4 - Remove woocommerce_catalog_ordering
+/**
+ * Add term attribute in product loop
+ */
+function ground_woocommerce_add_product_loop_term() {
+	global $product;
+	$product_id    = $product->get_id();
+	$product_terms = wc_get_product_terms(
+		$product_id,
+		'product_cat',
+		array( 'fields' => 'all' )
+	); // For attribute use "pa_nameofattribute.
 
-    =============================================================================  */
+	if ( count( $product_terms ) ) {
+		echo '<div class="product__terms">';
+		foreach ( $product_terms as $term ) {
+			$term_name = "$term->name";
+			if ( next( $product_terms ) == true ) {
+				$term_name .= ',';
+			}
+			echo $term_name;
+		}
+			echo '</div>';
+	}
+}
 
+// add_action( 'woocommerce_after_shop_loop_item_title', 'ground_woocommerce_add_product_loop_term', 9 );
 
-/*  ==========================================================================
-	1 - Remove loop add to cart button
-	==========================================================================  */
+/**
+ * Remove, Rename, Reorder or Add Custom Sorting Options
+ *
+ * @param [type] $options
+ * @return void
+ */
+function ground_woocommerce_customize_product_sorting( $options ) {
+	$options = array(
+		'menu_order' => __( 'Default sorting', 'woocommerce' ),
+		'popularity' => __( 'Sort by popularity', 'woocommerce' ),
+		'rating'     => __( 'Sort by average rating', 'woocommerce' ),
+		'date'       => __( 'Sort by newness', 'woocommerce' ),
+		'price'      => __( 'Sort by price: low to high', 'woocommerce' ),
+		'price-desc' => __( 'Sort by price: high to low', 'woocommerce' ),
+	);
 
-    // remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
+	// unset( $options[ 'popularity' ] );
+	// unset( $options[ 'menu_order' ] );
+	// unset( $options[ 'rating' ] );
+	// unset( $options[ 'date' ] );
+	// unset( $options[ 'price' ] );
+	// unset( $options[ 'price-desc' ] );
 
+	return $options;
+}
 
+// add_filter('woocommerce_catalog_orderby', 'ground_woocommerce_customize_product_sorting');
 
-/*  ==========================================================================
-	2 - Add term attribute in product loop
-	==========================================================================  */
-
-    function ground_woocommerce_add_product_loop_term() {
-        global $product;
-        $product_id = $product->get_id();
-        $product_terms = wc_get_product_terms($product_id, 'product_cat', array('fields' => 'all')); // For attribute use "pa_nameofattribute
-    
-        if (count($product_terms)) {
-            echo "<div class=\"product__terms\">";
-                foreach ($product_terms as $term) {
-                    $term_name = "$term->name";
-                    if (next($product_terms) == true) $term_name .= ",";
-                    echo $term_name;
-                }
-            echo "</div>";
-        }
-    }
-    
-    add_action('woocommerce_after_shop_loop_item_title', 'ground_woocommerce_add_product_loop_term', 9);
-
-
-
-/*  ==========================================================================
-	3 - Change Text catalog_orderby Button
-	==========================================================================  */
-
-    function ground_woocommerce_customize_product_sorting($sorting_options){
-        $sorting_options = array(
-            'menu_order' => __( 'Default sorting', 'woocommerce' ),
-            'popularity' => __( 'Sort by popularity', 'woocommerce' ),
-            'rating' => __( 'Sort by average rating', 'woocommerce' ),
-            'date' => __( 'Sort by newness', 'woocommerce' ),
-            'price' => __( 'Sort by price: low to high', 'woocommerce' ),
-            'price-desc' => __( 'Sort by price: high to low', 'woocommerce' ),
-        );
-
-        return $sorting_options;
-    }
-
-    //add_filter('woocommerce_catalog_orderby', 'ground_woocommerce_customize_product_sorting');
-
-
-
-/*  ==========================================================================
-	4 - Remove woocommerce_catalog_ordering
-	==========================================================================  */
-
-    // remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+/**
+ * Remove Product Sorting Dropdown
+ */
+// remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+// remove_action( 'woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10 );
