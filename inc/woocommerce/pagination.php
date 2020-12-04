2d2305ba13ebc986d4fac17aa606e9dbb448361b
Fabio Quarantini <wlospam@gmail.com>
2020-12-04 11:39:49 +0100

    Update pagination

--- inc/woocommerce/pagination.php
+++ inc/woocommerce/pagination.php
@@ -1,32 +1,28 @@
 <?php
+/**
+ * Pagination
+ *
+ * @package Ground
+ */
 
-/*  == PAGINATION ===========================================================
+/**
+ * WooCommerce custom pagination
+ */
+function ground_woocommerce_pagination() {
+	get_template_part( 'partials/pagination' );
+}
 
-        1 - WooCommerce custom pagination
-        2 - Change number of products that are displayed per page (shop page)
+remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+add_action( 'woocommerce_after_shop_loop', 'ground_woocommerce_pagination', 10 );
 
-    =============================================================================  */
+/**
+ * Change number of products that are displayed per page (shop page)
+ *
+ * @param int $cols Contains the current number of products per page based on the value stored on Options -> Reading.
+ */
+function ground_woocommerce_products_per_page( $cols ) {
+	$cols = 9;
+	return $cols;
+}
 
-
-/*  ==========================================================================
-	1 - WooCommerce custom pagination
-	==========================================================================  */
-
-    function ground_woocommerce_pagination() {
-        get_template_part( 'partials/pagination' );
-    }
-    
-    remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
-    add_action( 'woocommerce_after_shop_loop', 'ground_woocommerce_pagination', 10);
-
-
-
-/*  ==========================================================================
-	2 - Change number of products that are displayed per page (shop page)
-	==========================================================================  */
-
-    function ground_woocommerce_product_per_page( $cols ) {
-        return 30;
-    }
-
-    // add_filter( 'loop_shop_per_page', 'ground_woocommerce_product_per_page', 20 );
+// add_filter( 'loop_shop_per_page', 'ground_woocommerce_products_per_page', 20 );
