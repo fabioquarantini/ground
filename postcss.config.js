c8ec6dbc6529f1faacce4605a5f977ead6de5a41
Fabio Quarantini <wlospam@gmail.com>
2020-12-10 21:30:04 +0100

    Update postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-	}
+	plugins: [
+		require('tailwindcss'),
+		// require('postcss-100vh-fix'), // requires PostCSS 8
+		require('autoprefixer'),
+	]
 }
