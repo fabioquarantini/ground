af81e0f955633dcc42ea2272a7f5ea2808395294
Fabio Quarantini <wlospam@gmail.com>
2020-12-14 19:12:56 +0100

    Update header

--- partials/content-header.php
+++ partials/content-header.php
@@ -1,16 +1,16 @@
-<header class="header shadow-lg">
+<header class="flex justify-between items-center shadow-lg bg-white fixed top-0 w-full z-10 lg:px-8">
 
-	<a class="logo js-cursor-hide js-magnet" href="<?php echo esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
+	<a class="js-cursor-hide" href="<?php echo esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
 		<?php /* <img class="logo__img" src="<?php echo TEMPLATE_URL ?>/img/logo.svg" alt="<?php bloginfo('name'); ?>" /> */ ?>
 		<?php echo file_get_contents(TEMPLATE_PATH . "/img/logo.svg"); ?>
-	</a> <!-- End .logo -->
+	</a> <!-- End logo -->
 
 	<?php if ( !is_checkout() ) { ?>
 		<?php get_template_part('partials/navigation', 'primary'); ?>
 
-		<button class="header__search js-toggle js-magnet js-cursor-hover" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
+		<button class="header__search js-toggle js-cursor-hover" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
 			<?php ground_icon( 'search'); ?>
-		</button> <!-- End .navicon -->
+		</button> <!-- End icon search -->
 
 		<?php get_template_part('partials/woocommerce/minicart'); ?>
 
@@ -19,4 +19,4 @@
 		</button> <!-- End .navicon -->
 	<?php } ?>
 
-</header> <!-- End .header -->
+</header> <!-- End header -->
