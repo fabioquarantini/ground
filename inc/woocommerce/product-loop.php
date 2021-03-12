c35957e8ac0bf339f70b95e94db3c9f9819854b9
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-03-12 09:36:53 +0100

    WooCommerce: Change the DIV for WooCommerce product loop start and end

--- inc/woocommerce/product-loop.php
+++ inc/woocommerce/product-loop.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Products loop
  *
@@ -13,25 +14,26 @@
 /**
  * Add term attribute in product loop
  */
-function ground_woocommerce_add_product_loop_term() {
+function ground_woocommerce_add_product_loop_term()
+{
 	global $product;
 	$product_id    = $product->get_id();
 	$product_terms = wc_get_product_terms(
 		$product_id,
 		'product_cat',
-		array( 'fields' => 'all' )
+		array('fields' => 'all')
 	); // For attribute use "pa_nameofattribute.
 
-	if ( count( $product_terms ) ) {
+	if (count($product_terms)) {
 		echo '<div class="product__terms">';
-		foreach ( $product_terms as $term ) {
+		foreach ($product_terms as $term) {
 			$term_name = "$term->name";
-			if ( next( $product_terms ) == true ) {
+			if (next($product_terms) == true) {
 				$term_name .= ',';
 			}
 			echo $term_name;
 		}
-			echo '</div>';
+		echo '</div>';
 	}
 }
 
@@ -43,14 +45,15 @@ function ground_woocommerce_add_product_loop_term() {
  * @param [type] $options
  * @return void
  */
-function ground_woocommerce_customize_product_sorting( $options ) {
+function ground_woocommerce_customize_product_sorting($options)
+{
 	$options = array(
-		'menu_order' => __( 'Default sorting', 'woocommerce' ),
-		'popularity' => __( 'Sort by popularity', 'woocommerce' ),
-		'rating'     => __( 'Sort by average rating', 'woocommerce' ),
-		'date'       => __( 'Sort by newness', 'woocommerce' ),
-		'price'      => __( 'Sort by price: low to high', 'woocommerce' ),
-		'price-desc' => __( 'Sort by price: high to low', 'woocommerce' ),
+		'menu_order' => __('Default sorting', 'woocommerce'),
+		'popularity' => __('Sort by popularity', 'woocommerce'),
+		'rating'     => __('Sort by average rating', 'woocommerce'),
+		'date'       => __('Sort by newness', 'woocommerce'),
+		'price'      => __('Sort by price: low to high', 'woocommerce'),
+		'price-desc' => __('Sort by price: high to low', 'woocommerce'),
 	);
 
 	// unset( $options[ 'popularity' ] );
@@ -75,3 +78,19 @@ function ground_woocommerce_customize_product_sorting( $options ) {
  * Remove result count
  */
 // remove_action( 'woocommerce_before_shop_loop' , 'woocommerce_result_count', 20 );
+
+
+
+/**
+ * Change the DIV for WooCommerce product loop start and end
+ */
+
+function woocommerce_product_loop_start()
+{
+	echo '<div class="products grid grid-cols-1 lg:grid-cols-3 gap-4 js-infinite-container">';
+}
+
+function woocommerce_product_loop_end()
+{
+	echo '</div>';
+}
