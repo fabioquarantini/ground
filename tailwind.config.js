56c58e0495cb07e992259a185abe90ee74e4242b
Fabio Quarantini <wlospam@gmail.com>
2021-03-30 18:29:00 +0200

    Add tailwind line clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -110,6 +110,7 @@ module.exports = {
 	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/line-clamp'),
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/typography'),
 		require('tailwindcss-debug-screens'),
