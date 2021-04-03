e4300ac8a0462a351a8ad74ef1c4cbdc48b27ae1
Fabio Quarantini <wlospam@gmail.com>
2021-04-03 21:39:43 +0200

    Update export

--- js/utilities/cookies.js
+++ js/utilities/cookies.js
@@ -1,49 +1,46 @@
 /**
- * Utilities module
+ * Cookies
  */
 
-export default class Cookies {
-	/**
-	 * Create cookie
-	 * @param {string} name - Cookie name
-	 * @param {string} value - Cookie value
-	 * @param {number} days - Expiry date in days (`7`)
-	 */
-	static setCookie(name, value, days) {
-		let expires
+/**
+ * Create cookie
+ * @param {string} name - Cookie name
+ * @param {string} value - Cookie value
+ * @param {number} days - Expiry date in days (`7`)
+ */
+export function setCookie(name, value, days) {
+	let expires
 
-		if (days) {
-			const date = new Date()
-			date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
-			expires = `; expires=${date.toGMTString()}`
-		} else {
-			expires = ''
-		}
-
-		document.cookie = `${name}=${value}${expires}; path=/`
+	if (days) {
+		const date = new Date()
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+		expires = `; expires=${date.toGMTString()}`
+	} else {
+		expires = ''
 	}
 
-	/**
-	 * Read cookie
-	 * @param {string} name - Cookie name
-	 */
-	static getCookie(name) {
-		const nameEQ = `${name}=`
-		const ca = document.cookie.split(';')
-		for (let i = 0; i < ca.length; i++) {
-			let c = ca[i]
-			while (c.charAt(0) === ' ') c = c.substring(1, c.length)
-			if (c.indexOf(nameEQ) === 0)
-				return c.substring(nameEQ.length, c.length)
-		}
-		return null
-	}
+	document.cookie = `${name}=${value}${expires}; path=/`
+}
 
-	/**
-	 * Delete cookie
-	 * @param {string} name - Cookie name
-	 */
-	static deleteCookie(name) {
-		this.setCookie(name, '', -1)
+/**
+ * Read cookie
+ * @param {string} name - Cookie name
+ */
+export function getCookie(name) {
+	const nameEQ = `${name}=`
+	const ca = document.cookie.split(';')
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i]
+		while (c.charAt(0) === ' ') c = c.substring(1, c.length)
+		if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
 	}
+	return null
+}
+
+/**
+ * Delete cookie
+ * @param {string} name - Cookie name
+ */
+export function deleteCookie(name) {
+	this.setCookie(name, '', -1)
 }
