e6bf89a75816f7729794589c2677c8b4c75cdbb2
Fabio Quarantini <wlospam@gmail.com>
2021-04-03 21:51:02 +0200

    Update export

--- js/utilities/device.js
+++ js/utilities/device.js
@@ -2,75 +2,73 @@
  * Device
  */
 
-export default class Utilities {
-	/**
-	 * Match CSS media queries and JavaScript window width
-	 * @see http://stackoverflow.com/a/11310353
-	 * @returns {Object}
-	 */
-	static getViewportSize() {
-		let e = window
-		let a = 'inner'
-		if (!('innerWidth' in window)) {
-			a = 'client'
-			e = document.documentElement || document.body
-		}
-		return {
-			width: e[`${a}Width`],
-			height: e[`${a}Height`],
-		}
+/**
+ * Match CSS media queries and JavaScript window width
+ * @see http://stackoverflow.com/a/11310353
+ * @returns {Object}
+ */
+export function getViewportSize() {
+	let e = window
+	let a = 'inner'
+	if (!('innerWidth' in window)) {
+		a = 'client'
+		e = document.documentElement || document.body
 	}
-
-	/**
-	 * Detect Touch Events
-	 * @see http://www.stucox.com/blog/you-cant-detect-a-touchscreen/
-	 * @returns {boolean}
-	 */
-	static isTouch() {
-		// eslint-disable-next-line no-undef
-		return (
-			('ontouchstart' in window ||
-				(window.DocumentTouch && document instanceof DocumentTouch)) ===
-			true
-		)
+	return {
+		width: e[`${a}Width`],
+		height: e[`${a}Height`],
 	}
+}
 
-	/**
-	 * Get device orientation
-	 * @returns {string}
-	 */
-	static getDeviceOrientation() {
-		const mql = window.matchMedia('(orientation: portrait)')
-		return mql.matches ? 'portrait' : 'landscape'
+/**
+ * Detect Touch Events
+ * @see http://www.stucox.com/blog/you-cant-detect-a-touchscreen/
+ * @returns {boolean}
+ */
+export function isTouch() {
+	// eslint-disable-next-line no-undef
+	return (
+		('ontouchstart' in window ||
+			(window.DocumentTouch && document instanceof DocumentTouch)) ===
+		true
+	)
+}
+
+/**
+ * Get device orientation
+ * @returns {string}
+ */
+export function getDeviceOrientation() {
+	const mql = window.matchMedia('(orientation: portrait)')
+	return mql.matches ? 'portrait' : 'landscape'
+}
+
+/**
+ * Cross-browser mouse position
+ * @param {Object} event - Event
+ * @returns {Object}
+ * @see http://www.quirksmode.org/js/events_properties.html#position
+ */
+export function getMousePosition(event) {
+	let posx = 0
+	let posy = 0
+	// eslint-disable-next-line no-param-reassign
+	if (!event) event = window.event
+	if (event.pageX || event.pageY) {
+		posx = event.pageX
+		posy = event.pageY
+	} else if (event.clientX || event.clientY) {
+		posx =
+			event.clientX +
+			document.body.scrollLeft +
+			document.documentElement.scrollLeft
+		posy =
+			event.clientY +
+			document.body.scrollTop +
+			document.documentElement.scrollTop
 	}
-
-	/**
-	 * Cross-browser mouse position
-	 * @param {Object} event - Event
-	 * @returns {Object}
-	 * @see http://www.quirksmode.org/js/events_properties.html#position
-	 */
-	static getMousePosition(event) {
-		let posx = 0
-		let posy = 0
-		// eslint-disable-next-line no-param-reassign
-		if (!event) event = window.event
-		if (event.pageX || event.pageY) {
-			posx = event.pageX
-			posy = event.pageY
-		} else if (event.clientX || event.clientY) {
-			posx =
-				event.clientX +
-				document.body.scrollLeft +
-				document.documentElement.scrollLeft
-			posy =
-				event.clientY +
-				document.body.scrollTop +
-				document.documentElement.scrollTop
-		}
-		return {
-			x: posx,
-			y: posy,
-		}
+	return {
+		x: posx,
+		y: posy,
 	}
 }
