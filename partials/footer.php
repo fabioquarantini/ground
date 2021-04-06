30bed37ff3c1cb4a571c8c7644f7006e8c3b6914
Fabio Quarantini <wlospam@gmail.com>
2021-04-06 17:27:31 +0200

    Remove google analytics

--- partials/footer.php
+++ partials/footer.php
@@ -14,18 +14,7 @@
 	<?php get_template_part( 'partials/cursor' ); ?>
 	<?php get_template_part( 'partials/modal' ); ?>
 
-	<?php
-	wp_footer();
-
-	// Google Analytics: change UA-XXXXX-Y to be your site's ID.
-	?>
-	<script>
-		window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-		ga('create','UA-XXXXX-Y','auto');
-		ga('set', 'anonymizeIp', true);
-		ga('send','pageview')
-	</script>
-	<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+	<?php wp_footer(); ?>
 
 	</body>
 </html>
