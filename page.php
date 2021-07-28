f5870a74d7cce3cb1d14d6ef92b3b001d9345cfe
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-07-28 09:42:54 +0200

    Fix if (class_exists('WooCommerce'))

--- page.php
+++ page.php
@@ -1,48 +1,42 @@
 <?php
+
 /**
  * Pages
  *
  * @package Ground
  */
 
-get_template_part( 'partials/header' );
+get_template_part('partials/header');
 ?>
 
-	<div class="container
-		<?php
-		if ( is_cart() || is_account_page() ) {
-			echo 'max-w-screen-lg';
-		} if ( is_checkout() ) {
-			echo 'max-w-screen-md'; }
-		?>
-	">
+<div class="container max-w-screen-lg">
 
-		<?php get_template_part( 'partials/breadcrumbs' ); ?>
+	<?php get_template_part('partials/breadcrumbs'); ?>
 
-		<div class="lg:flex lg:flex-wrap">
+	<div class="lg:flex lg:flex-wrap">
 
-			<?php if ( is_checkout() || is_cart() || is_account_page() ) { ?>
-				<div class="w-full">
+		<?php if (class_exists('WooCommerce') && (is_checkout() || is_cart() || is_account_page())) { ?>
+			<div class="w-full">
 			<?php } else { ?>
 
 				<div class="lg:w-3/12 lg:pr-8">
-					<?php get_template_part( 'partials/sidebar', 'primary' ); ?>
+					<?php get_template_part('partials/sidebar', 'primary'); ?>
 				</div>
 
 				<div class="lg:w-9/12">
-			<?php } ?>
+				<?php } ?>
 
 				<?php
-				while ( have_posts() ) :
+				while (have_posts()) :
 					the_post();
 
-					get_template_part( 'partials/content', 'page' );
+					get_template_part('partials/content', 'page');
 
 				endwhile;
 				?>
-			</div>
-		</div> <!-- End .row -->
+				</div>
+			</div> <!-- End .row -->
 	</div> <!-- End .container -->
 
-<?php
-get_template_part( 'partials/footer' );
+	<?php
+	get_template_part('partials/footer');
--- partials/content-header.php
+++ partials/content-header.php
@@ -1,18 +1,20 @@
 <header class="flex justify-between items-center shadow-lg bg-white fixed top-0 w-full z-10 px-8 py-6">
 
-	<a class="js-cursor-hide" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
+	<a class="js-cursor-hide" href="<?php echo esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
 		<?php /* <img class="logo__img" src="<?php echo TEMPLATE_URL ?>/img/logo.svg" alt="<?php bloginfo('name'); ?>" /> */ ?>
-		<?php echo file_get_contents( TEMPLATE_PATH . '/img/logo.svg' ); ?>
+		<?php echo file_get_contents(TEMPLATE_PATH . '/img/logo.svg'); ?>
 	</a> <!-- End logo -->
 
-	<?php if ( ! is_checkout() ) { ?>
-		<?php get_template_part( 'partials/navigation', 'primary' ); ?>
+	<?php
+
+	if (class_exists('WooCommerce') && !is_checkout()) { ?>
+		<?php get_template_part('partials/navigation', 'primary'); ?>
 
 		<button class="header__search js-toggle js-cursor-hover" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
-			<?php ground_icon( 'search' ); ?>
+			<?php ground_icon('search'); ?>
 		</button> <!-- End .header__search -->
 
-		<?php get_template_part( 'partials/woocommerce/minicart' ); ?>
+		<?php get_template_part('partials/woocommerce/minicart'); ?>
 
 		<button class="navicon js-toggle" data-toggle-target="body" data-toggle-class-name="is-navigation-open">
 			<i class="navicon__icon"></i>
