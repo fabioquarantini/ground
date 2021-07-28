2ba6d56427effce75eec7d919c7c7cbb5fe1054b
Fabio Quarantini <wlospam@gmail.com>
2021-07-28 18:41:10 +0200

    Update coding standards

--- inc/variables.php
+++ inc/variables.php
@@ -1,20 +1,20 @@
 <style type="text/css">
-    :root {
-        <?php if (defined('COLOR_PRIMARY') && COLOR_PRIMARY) {
-            echo '--ground-color-primary:' . COLOR_PRIMARY . ';';
-        };
-        if (defined('COLOR_SECONDARY') && COLOR_SECONDARY) {
-            echo '--ground-color-secondary:' . COLOR_SECONDARY . ';';
-        };
-        if (defined('FONT_PRIMARY_NAME') && FONT_PRIMARY_NAME) {
-            echo '--ground-font-primary:' . FONT_PRIMARY_NAME . ';';
-        };
-        if (defined('FONT_SECONDARY_NAME') && FONT_SECONDARY_NAME) {
-            echo '--ground-font-secondary:' . FONT_SECONDARY_NAME . ';';
-        };
-        if (defined('ROUNDED_THEME') && ROUNDED_THEME) {
-            echo '--ground-rounded-theme:' . ROUNDED_THEME . 'px;';
-        };
-        ?>
-    }
+	:root {
+		<?php if ( defined( 'COLOR_PRIMARY' ) && COLOR_PRIMARY ) {
+			echo '--ground-color-primary:' . COLOR_PRIMARY . ';';
+		};
+		if ( defined( 'COLOR_SECONDARY' ) && COLOR_SECONDARY ) {
+			echo '--ground-color-secondary:' . COLOR_SECONDARY . ';';
+		};
+		if ( defined( 'FONT_PRIMARY_NAME' ) && FONT_PRIMARY_NAME ) {
+			echo '--ground-font-primary:' . FONT_PRIMARY_NAME . ';';
+		};
+		if ( defined( 'FONT_SECONDARY_NAME' ) && FONT_SECONDARY_NAME ) {
+			echo '--ground-font-secondary:' . FONT_SECONDARY_NAME . ';';
+		};
+		if ( defined( 'ROUNDED_THEME' ) && ROUNDED_THEME ) {
+			echo '--ground-rounded-theme:' . ROUNDED_THEME . 'px;';
+		};
+		?>
+	}
 </style>
