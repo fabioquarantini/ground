f6c4aa5d75c1d33ff4507da7a0fa8143fac6ae78
Fabio Quarantini <wlospam@gmail.com>
2021-07-29 13:25:14 +0200

    Re-enable emojis

--- inc/head-output.php
+++ inc/head-output.php
@@ -78,59 +78,6 @@ function ground_head_output() {
 add_action( 'init', 'ground_head_output' );
 
 /**
- * Disable emojis
- */
-function ground_disable_emojis() {
-
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	add_filter( 'tiny_mce_plugins', 'ground_disable_emojis_tinymce' );
-	add_filter( 'wp_resource_hints', 'ground_disable_emojis_remove_dns_prefetch', 10, 2 );
-
-}
-
-/**
- * Disable emojis TinyMCE
- *
- * @param array $plugins An array of default TinyMCE plugins.
- * @return array
- */
-function ground_disable_emojis_tinymce( $plugins ) {
-
-	if ( is_array( $plugins ) ) {
-		return array_diff( $plugins, array( 'wpemoji' ) );
-	} else {
-		return array();
-	}
-
-}
-
-/**
- * Disable emojis DNS prefetch
- *
- * @param array  $urls URLs to print for resource hints.
- * @param string $relation_type The relation type the URLs are printed for, e.g. 'preconnect' or 'prerender'.
- * @return array
- */
-function ground_disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
-
-	if ( 'dns-prefetch' === $relation_type ) {
-		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
-		$urls          = array_diff( $urls, array( $emoji_svg_url ) );
-	}
-
-	return $urls;
-
-}
-
-add_action( 'init', 'ground_disable_emojis' );
-
-/**
  * Remove login logo
  */
 function ground_login_css() { ?>
