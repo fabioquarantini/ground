26ae2585181d922d7df87fafc146ce39bd9d0940
Fabio Quarantini <wlospam@gmail.com>
2021-07-29 13:17:38 +0200

    Add verison

--- inc/constants.php
+++ inc/constants.php
@@ -6,6 +6,7 @@
  * @package Ground
  */
 
+define( 'GROUND_VERSION', '2.0.0' );
 define( 'GROUND_SITE_URL', site_url() ); // Return http://www.site.com.
 define( 'GROUND_TEMPLATE_URL', get_template_directory_uri() ); // Return http://www.site.com/wp-content/themes/themename.
 define( 'GROUND_TEMPLATE_PATH', get_template_directory() ); // Return /home/user/public_html/wp-content/themes/themename.
