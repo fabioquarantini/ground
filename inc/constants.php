e362f9dd824399cc3624dad75e5af40320b4def5
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-03 18:06:07 +0200

    Update ACF Vars

--- inc/constants.php
+++ inc/constants.php
@@ -6,54 +6,55 @@
  * @package Ground
  */
 
-define( 'GROUND_VERSION', '2.0.0' );
-define( 'GROUND_SITE_URL', site_url() ); // Return http://www.site.com.
-define( 'GROUND_TEMPLATE_URL', get_template_directory_uri() ); // Return http://www.site.com/wp-content/themes/themename.
-define( 'GROUND_TEMPLATE_PATH', get_template_directory() ); // Return /home/user/public_html/wp-content/themes/themename.
-define( 'GROUND_CHARSET', get_bloginfo( 'charset' ) ); // The "Encoding for pages and feeds" (set in Settings > Reading).
+define('GROUND_VERSION', '2.0.0');
+define('GROUND_SITE_URL', site_url()); // Return http://www.site.com.
+define('GROUND_TEMPLATE_URL', get_template_directory_uri()); // Return http://www.site.com/wp-content/themes/themename.
+define('GROUND_TEMPLATE_PATH', get_template_directory()); // Return /home/user/public_html/wp-content/themes/themename.
+define('GROUND_CHARSET', get_bloginfo('charset')); // The "Encoding for pages and feeds" (set in Settings > Reading).
 
 // Colors.
-define( 'GROUND_COLOR_PRIMARY', get_field( 'color_primary', 'option' ) ? get_field( 'color_primary', 'option' ) : '#6366F1' );
-define( 'GROUND_COLOR_SECONDARY', get_field( 'color_secondary', 'option' ) ? get_field( 'color_secondary', 'option' ) : '#14B8A6' );
+define('GROUND_COLOR_PRIMARY', get_field('color_primary', 'option') ? get_field('color_primary', 'option') : '#6366F1');
+define('GROUND_COLOR_SECONDARY', get_field('color_secondary', 'option') ? get_field('color_secondary', 'option') : '#14B8A6');
 
 // Fonts.
-define( 'GROUND_FONT_PRIMARY', get_field( 'font_primary', 'option' ) ? get_field( 'font_primary', 'option' ) : '' );
-define( 'GROUND_FONT_PRIMARY_NAME', get_field( 'font_primary_name', 'option' ) ? get_field( 'font_primary_name', 'option' ) : '' );
-define( 'GROUND_FONT_SECONDARY', get_field( 'font_secondary', 'option' ) ? get_field( 'font_secondary', 'option' ) : '' );
-define( 'GROUND_FONT_SECONDARY_NAME', get_field( 'font_secondary_name', 'option' ) ? get_field( 'font_secondary_name', 'option' ) : '' );
+define('GROUND_FONT_SOURCE_PRIMARY', get_field('font_source_primary', 'option') ? get_field('font_source_primary', 'option') : '');
+define('GROUND_FONT_FAMILY_PRIMARY', get_field('font_family_primary', 'option') ? get_field('font_family_primary', 'option') : '');
+define('GROUND_FONT_SOURCE_SECONDARY', get_field('font_source_secondary', 'option') ? get_field('font_source_secondary', 'option') : '');
+define('GROUND_FONT_FAMILY_SECONDARY', get_field('font_family_secondary', 'option') ? get_field('font_family_secondary', 'option') : '');
 
 // Styles.
-define( 'GROUND_ROUNDED_THEME', get_field( 'rounded_theme', 'option' ) ? get_field( 'rounded_theme', 'option' ) : '0' );
+define('GROUND_ROUNDED_THEME', get_field('rounded_theme', 'option') ? get_field('rounded_theme', 'option') : '0');
 
 // Company.
-/**
- * TODO: Lat lang
- */
-define( 'GROUND_COMPANY_ADDRESS', get_field( 'company_address', 'option' ) ? get_field( 'company_address', 'option' ) : '' );
-define( 'GROUND_COMPANY_ADDRESS_MAPS', get_field( 'company_address_maps', 'option' ) ? get_field( 'company_address_maps', 'option' ) : '' );
-define( 'GROUND_COMPANY_PHONE_PRIMARY', get_field( 'company_phone_primary', 'option' ) ? get_field( 'company_phone_primary', 'option' ) : '' );
-define( 'GROUND_COMPANY_PHONE_SECONDARY', get_field( 'company_phone_secondary', 'option' ) ? get_field( 'company_phone_secondary', 'option' ) : '' );
-define( 'GROUND_COMPANY_EMAIL_PRIMARY', get_field( 'company_email_primary', 'option' ) ? get_field( 'company_email_primary', 'option' ) : '' );
-define( 'GROUND_COMPANY_EMAIL_SECONDARY', get_field( 'company_email_secondary', 'option' ) ? get_field( 'company_email_secondary', 'option' ) : '' );
-define( 'GROUND_COMPANY_FAX', get_field( 'company_fax', 'option' ) ? get_field( 'company_fax', 'option' ) : '' );
+define('GROUND_COMPANY_ADDRESS', get_field('company_address', 'option') ? get_field('company_address', 'option') : '');
+define('GROUND_COMPANY_ADDRESS_URL', get_field('company_address_url', 'option') ? get_field('company_address_url', 'option') : '');
+define('GROUND_COMPANY_ADDRESS_LATITUDE', get_field('company_address_latitude', 'option') ? get_field('company_address_latitude', 'option') : '');
+define('GROUND_COMPANY_ADDRESS_LONGITUDE', get_field('company_address_longitude', 'option') ? get_field('company_address_longitude', 'option') : '');
+define('GROUND_COMPANY_PHONE_PRIMARY', get_field('company_phone_primary', 'option') ? get_field('company_phone_primary', 'option') : '');
+define('GROUND_COMPANY_PHONE_SECONDARY', get_field('company_phone_secondary', 'option') ? get_field('company_phone_secondary', 'option') : '');
+define('GROUND_COMPANY_EMAIL_PRIMARY', get_field('company_email_primary', 'option') ? get_field('company_email_primary', 'option') : '');
+define('GROUND_COMPANY_EMAIL_SECONDARY', get_field('company_email_secondary', 'option') ? get_field('company_email_secondary', 'option') : '');
+define('GROUND_COMPANY_FAX', get_field('company_fax', 'option') ? get_field('company_fax', 'option') : '');
 
-/**
- * TODO: Rename es GROUND_FACEBOOK_URL
- */
 // Socials.
-define( 'GROUND_SOCIAL_FACEBOOK', get_field( 'social_facebook', 'option' ) ? get_field( 'social_facebook', 'option' ) : '' );
-define( 'GROUND_SOCIAL_TWITTER', get_field( 'social_twitter', 'option' ) ? get_field( 'social_twitter', 'option' ) : '' );
-define( 'GROUND_SOCIAL_YOUTUBE', get_field( 'social_youtube', 'option' ) ? get_field( 'social_youtube', 'option' ) : '' );
-define( 'GROUND_SOCIAL_FLICKR', get_field( 'social_flickr', 'option' ) ? get_field( 'social_flickr', 'option' ) : '' );
-define( 'GROUND_SOCIAL_INSTAGRAM', get_field( 'social_instagram', 'option' ) ? get_field( 'social_instagram', 'option' ) : '' );
-define( 'GROUND_SOCIAL_LINKEDIN', get_field( 'social_linkedin', 'option' ) ? get_field( 'social_linkedin', 'option' ) : '' );
+define('GROUND_SOCIAL_FACEBOOK_URL', get_field('social_facebook_url', 'option') ? get_field('social_facebook_url', 'option') : '');
+define('GROUND_SOCIAL_TWITTER_URL', get_field('social_twitter_url', 'option') ? get_field('social_twitter_url', 'option') : '');
+define('GROUND_SOCIAL_YOUTUBE_URL', get_field('social_youtube_url', 'option') ? get_field('social_youtube_url', 'option') : '');
+define('GROUND_SOCIAL_FLICKR_URL', get_field('social_flickr_url', 'option') ? get_field('social_flickr_url', 'option') : '');
+define('GROUND_SOCIAL_INSTAGRAM_URL', get_field('social_instagram_url', 'option') ? get_field('social_instagram_url', 'option') : '');
+define('GROUND_SOCIAL_LINKEDIN_URL', get_field('social_linkedin_url', 'option') ? get_field('social_linkedin_url', 'option') : '');
 
 // Footer.
-define( 'GROUND_FOOTER_COPYRIGHT', get_field( 'footer_copyright', 'option' ) ? get_field( 'footer_copyright', 'option' ) : '' );
+define('GROUND_FOOTER_COPYRIGHT', get_field('footer_copyright', 'option') ? get_field('footer_copyright', 'option') : '');
 
-// Logo.
-define( 'GROUND_LOGO_PRIMARY_PATH', get_field( 'logo_primary_path', 'option' ) ? get_field( 'logo_primary', 'option' ) : GROUND_TEMPLATE_PATH . '/img/logo.svg' );
-define( 'GROUND_LOGO_SECONDARY_PATH', get_field( 'logo_secondary_path', 'option' ) ? get_field( 'logo_secondary_path', 'option' ) : GROUND_TEMPLATE_PATH . '/img/logo.svg' );
+// Logos.
+define('GROUND_LOGO_PRIMARY_URL', get_field('logo_primary_url', 'option') ? get_field('logo_primary_url', 'option') : GROUND_TEMPLATE_PATH . '/img/logo.svg');
+define('GROUND_LOGO_PRIMARY_SOURCE', get_field('logo_primary_source', 'option') ? get_field('logo_primary_source', 'option') : GROUND_TEMPLATE_PATH . '/img/logo.svg');
+define('GROUND_LOGO_PRIMARY_TYPE', get_field('logo_primary_type', 'option') ? get_field('logo_primary_type', 'option') : GROUND_TEMPLATE_PATH . '/img/logo.svg');
+
+define('GROUND_LOGO_SECONDARY_URL', get_field('logo_secondary_url', 'option') ? get_field('logo_secondary_url', 'option') : GROUND_TEMPLATE_PATH . '/img/logo.svg');
+define('GROUND_LOGO_SECONDARY_SOURCE', get_field('logo_secondary_source', 'option') ? get_field('logo_secondary_source', 'option') : GROUND_TEMPLATE_PATH . '/img/logo.svg');
+define('GROUND_LOGO_SECONDARY_TYPE', get_field('logo_secondary_type', 'option') ? get_field('logo_secondary_type', 'option') : GROUND_TEMPLATE_PATH . '/img/logo.svg');
 
 // No image.
-define( 'GROUND_NO_IMAGE_PATH', get_field( 'no_image_path', 'option' ) ? get_field( 'no_image_path', 'option' ) : GROUND_TEMPLATE_URL . '/img/no-image.svg' );
+define('GROUND_NO_IMAGE_PATH', get_field('no_image_path', 'option') ? get_field('no_image_path', 'option') : GROUND_TEMPLATE_URL . '/img/no-image.svg');
--- partials/content-header.php
+++ partials/content-header.php
@@ -1,21 +1,26 @@
 <header class="flex justify-between items-center shadow-lg bg-white fixed top-0 w-full z-10 px-8 py-6">
 
-	<a class="js-cursor-hide" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
-		<?php /* <img class="logo__img" src="<?php echo GROUND_LOGO_PRIMARY_PATH" alt="<?php bloginfo('name'); ?>" /> */ ?>
-		<?php echo file_get_contents( GROUND_LOGO_PRIMARY_PATH ); ?>
+	<a class="js-cursor-hide" href="<?php echo esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
+
+		<?php if (GROUND_LOGO_PRIMARY_TYPE == '1') {
+			echo GROUND_LOGO_PRIMARY_SOURCE;
+		} else { ?>
+			<img class="w-20 h-auto" src="<?php echo esc_url(GROUND_LOGO_PRIMARY_URL['sizes']['medium']); ?>" alt="<?php echo esc_attr(GROUND_LOGO_PRIMARY_URL['alt']); ?>" />
+		<?php } ?>
+
 	</a> <!-- End logo -->
 
 	<?php
 
-	if ( class_exists( 'WooCommerce' ) && ! is_checkout() ) {
-		?>
-		<?php get_template_part( 'partials/navigation', 'primary' ); ?>
+	if (class_exists('WooCommerce') && !is_checkout()) {
+	?>
+		<?php get_template_part('partials/navigation', 'primary'); ?>
 
 		<button class="header__search js-toggle js-cursor-hover" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
-			<?php ground_icon( 'search' ); ?>
+			<?php ground_icon('search'); ?>
 		</button> <!-- End .header__search -->
 
-		<?php get_template_part( 'partials/woocommerce/minicart' ); ?>
+		<?php get_template_part('partials/woocommerce/minicart'); ?>
 
 		<button class="navicon js-toggle" data-toggle-target="body" data-toggle-class-name="is-navigation-open">
 			<i class="navicon__icon"></i>
