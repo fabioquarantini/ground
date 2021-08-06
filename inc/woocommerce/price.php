957a56751d14eac171ccfeb2325a620424834ac4
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-06 11:36:18 +0200

    Add Show savings at the cart. Add Show sale prices in the cart.

--- inc/woocommerce/price.php
+++ inc/woocommerce/price.php
@@ -38,3 +38,51 @@ function ground_woocommerce_variation_price_format($wc_format_price_range, $prod
 }
 
 add_filter('woocommerce_variable_price_html', 'ground_woocommerce_variation_price_format', 10, 2);
+
+
+
+/**
+ * Show sale prices in the cart.
+ */
+function ground_show_sale_price_at_cart($old_display, $cart_item, $cart_item_key)
+{
+
+	/** @var WC_Product $product */
+	$product = $cart_item['data'];
+
+	if ($product) {
+		return $product->get_price_html();
+	}
+
+	return $old_display;
+}
+add_filter('woocommerce_cart_item_price', 'ground_show_sale_price_at_cart', 10, 3);
+
+
+
+/**
+ * Show savings at the cart.
+ */
+function ground_buy_now_save_x_cart()
+{
+
+	$savings = 0;
+
+	foreach (WC()->cart->get_cart() as $key => $cart_item) {
+		/** @var WC_Product $product */
+		$product = $cart_item['data'];
+
+		if ($product->is_on_sale()) {
+			$savings += ($product->get_regular_price() - $product->get_sale_price()) * $cart_item['quantity'];
+		}
+	}
+
+	if (!empty($savings)) { ?>
+		<tr class="order-savings">
+			<th><?php _e('Your savings'); ?></th>
+			<td data-title="<?php _e('Your savings'); ?>"><?php echo sprintf(__('Buy now and save %s!'), wc_price($savings)); ?></td>
+		</tr>
+<?php
+	}
+}
+add_action('woocommerce_cart_totals_before_order_total', 'ground_buy_now_save_x_cart');
