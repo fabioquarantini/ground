805d04bfd361242059a1ecaf0a9b38ddc80d6cc8
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-09 14:03:20 +0200

    Add cart.js ( auto -pen minicart after added item )

---
+++ js/components/cart.js
@@ -0,0 +1,32 @@
+import * as deepmerge from 'deepmerge';
+
+export default class Cart {
+	/**
+	 * @param {string} element - Selector
+	 * @param {Object} options - User options
+	 */
+	constructor(element, options) {
+		this.element = element || '.minicart';
+		this.defaults = {
+			triggers: this.element
+		};
+		this.options = options ? deepmerge(this.defaults, options) : this.defaults;
+
+		window.addEventListener('DOMContentLoaded', () => {
+			this.init();
+		});
+	}
+
+	/**
+	 * Init
+	 */
+	init() {
+		this.DOM = {
+			element: document.querySelectorAll(this.element)
+		};
+
+		jQuery('body').on('added_to_cart', function (e, fragments, cart_hash, this_button) {
+			jQuery('html').addClass('is-minicart-open');
+		});
+	}
+}
