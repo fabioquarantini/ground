90b18a745edcf1d5f51011f3720a5cd2cc9066c0
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-09 14:29:33 +0200

    Add unique class in loop-start woocommerce

--- woocommerce/loop/loop-start.php
+++ woocommerce/loop/loop-start.php
@@ -20,4 +20,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<div class="products js-infinite-container columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
+<div class="ground-products products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?> js-infinite-container">
