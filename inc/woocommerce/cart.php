107c53f0132cac65680b26879a2bc6405a5489e5
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-10 15:44:09 +0200

    Add minicart

--- inc/woocommerce/cart.php
+++ inc/woocommerce/cart.php
@@ -13,25 +13,24 @@
  * @link https://github.com/woocommerce/woocommerce/blob/master/templates/cart/mini-cart.php
  * @link https://hotexamples.com/examples/-/-/woocommerce_cross_sell_display/php-woocommerce_cross_sell_display-function-examples.html
  */
-function ground_add_crosssells_minicart()
-{
-	wp_reset_query();
-	woocommerce_cross_sell_display(4, 2);
+function ground_add_crosssells_minicart() {
+	// wp_reset_query();
+	woocommerce_cross_sell_display( 3 );
 }
 
-add_action('woocommerce_mini_cart_contents', 'ground_add_crosssells_minicart');
+add_action( 'woocommerce_mini_cart_contents', 'ground_add_crosssells_minicart' );
 
 /**
  * Remove attributes in product title
  */
-add_filter('woocommerce_product_variation_title_include_attributes', '__return_false');
-add_filter('woocommerce_is_attribute_in_product_name', '__return_false');
+add_filter( 'woocommerce_product_variation_title_include_attributes', '__return_false' );
+add_filter( 'woocommerce_is_attribute_in_product_name', '__return_false' );
 
 /**
  * Move cross sells under cart table
  */
-remove_action('woocommerce_cart_collaterals', 'woocommerce_cross_sell_display');
-add_action('woocommerce_after_cart', 'woocommerce_cross_sell_display');
+remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
+add_action( 'woocommerce_after_cart', 'woocommerce_cross_sell_display' );
 
 
 
@@ -40,14 +39,14 @@ add_action('woocommerce_after_cart', 'woocommerce_cross_sell_display');
  *
  * @see ground_woocommerce_cart_link_fragment()
  */
-if (defined('WC_VERSION') && version_compare(WC_VERSION, '2.3', '>=')) {
-	add_filter('woocommerce_add_to_cart_fragments', 'ground_woocommerce_cart_link_fragment');
+if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '2.3', '>=' ) ) {
+	add_filter( 'woocommerce_add_to_cart_fragments', 'ground_woocommerce_cart_link_fragment' );
 } else {
-	add_filter('add_to_cart_fragments', 'ground_woocommerce_cart_link_fragment');
+	add_filter( 'add_to_cart_fragments', 'ground_woocommerce_cart_link_fragment' );
 }
 
 
-if (!function_exists('ground_woocommerce_cart_link_fragment')) {
+if ( ! function_exists( 'ground_woocommerce_cart_link_fragment' ) ) {
 	/**
 	 * Cart Fragments
 	 * Ensure cart contents update when products are added to the cart via AJAX
@@ -55,8 +54,7 @@ if (!function_exists('ground_woocommerce_cart_link_fragment')) {
 	 * @param  array $fragments Fragments to refresh via AJAX.
 	 * @return array            Fragments to refresh via AJAX
 	 */
-	function ground_woocommerce_cart_link_fragment($fragments)
-	{
+	function ground_woocommerce_cart_link_fragment( $fragments ) {
 		global $woocommerce;
 
 		ob_start();
@@ -68,7 +66,7 @@ if (!function_exists('ground_woocommerce_cart_link_fragment')) {
 }
 
 
-if (!function_exists('ground_woocommerce_cart_link')) {
+if ( ! function_exists( 'ground_woocommerce_cart_link' ) ) {
 	/**
 	 * Cart Link
 	 * Displayed a link to the cart including the number of items present and the cart total
@@ -76,29 +74,27 @@ if (!function_exists('ground_woocommerce_cart_link')) {
 	 * @return void
 	 * @since  1.0.0
 	 */
-	function ground_woocommerce_cart_link()
-	{
-		get_template_part('partials/woocommerce/minicart', 'contents');
+	function ground_woocommerce_cart_link() {
+		get_template_part( 'partials/woocommerce/minicart', 'contents' );
 	}
 }
 
 /**
  * Automatically Update Cart on Quantity Change
  */
-function ground_cart_refresh_update_qty()
-{
-	if (is_cart()) {
-?>
+function ground_cart_refresh_update_qty() {
+	if ( is_cart() ) {
+		?>
 		<script type="text/javascript">
 			jQuery('div.woocommerce').on('click', 'input.qty', function() {
 				jQuery("[name='update_cart']").trigger("click");
 			});
 		</script>
-<?php
+		<?php
 	}
 }
 
-add_action('wp_footer', 'ground_cart_refresh_update_qty');
+add_action( 'wp_footer', 'ground_cart_refresh_update_qty' );
 
 
 
@@ -106,12 +102,13 @@ add_action('wp_footer', 'ground_cart_refresh_update_qty');
 /**
  * Show SKU on cart pages
  */
-add_action('woocommerce_after_cart_item_name', 'ground_sku_below_cart_item_name', 11, 2);
+add_action( 'woocommerce_after_cart_item_name', 'ground_sku_below_cart_item_name', 11, 2 );
 
-function ground_sku_below_cart_item_name($cart_item, $cart_item_key)
-{
-	$_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
-	$sku = $_product->get_sku();
-	if (!$sku) return;
-	echo '<p class="mt-2"><small>' . __('Code', 'ground') . ': ' . $sku . '</small></p>';
+function ground_sku_below_cart_item_name( $cart_item, $cart_item_key ) {
+	$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+	$sku      = $_product->get_sku();
+	if ( ! $sku ) {
+		return;
+	}
+	echo '<p class="mt-2"><small>' . __( 'Code', 'ground' ) . ': ' . $sku . '</small></p>';
 }
--- partials/woocommerce/minicart.php
+++ partials/woocommerce/minicart.php
@@ -2,11 +2,13 @@
 
 	<div id="minicart" class="minicart">
 		<div class="minicart__panel js-toggle" data-toggle-target="html" data-toggle-class-name="is-minicart-open"></div>
-		<div class="minicart__close js-toggle" data-toggle-target="html" data-toggle-class-name="is-minicart-open">
-			<?php ground_icon('close'); ?>
+		<?php get_template_part( 'partials/woocommerce/minicart', 'contents' ); ?>
+		<div class="minicart__body">
+			<div class="minicart__close js-toggle" data-toggle-target="html" data-toggle-class-name="is-minicart-open">
+				<?php ground_icon( 'close' ); ?>
+			</div>
+			<?php the_widget( 'WC_Widget_Cart', 'title=Prodotti nel carrello&hide_if_empty=1' ); ?>
 		</div>
-		<?php get_template_part( 'partials/woocommerce/minicart','contents' ); ?>
-		<?php the_widget( 'WC_Widget_Cart', 'title=Prodotti nel carrello&hide_if_empty=1' ); ?>
 	</div>
 
 <?php endif; ?>
