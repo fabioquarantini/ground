b0e30dd2230751e30095bc0a590e23b2c5dfa2ff
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-12 16:03:37 +0200

    minor fix

--- partials/search-form-input.php
+++ partials/search-form-input.php
@@ -2,5 +2,5 @@
 	<input type="text" id="js-ajax-search-input" placeholder="<?php _e( 'Search products', 'ground' ); ?>" value="<?php the_search_query(); ?>" name="s" id="s" />
 	<button class="absolute top-3 right-4" type="submit"><?php ground_icon( 'search', 'form__icon' ); ?></button>
 
-	<div class="js-ajax-search-spinner animate-spin h-5 w-5 mr-3 absolute top-3 -right-8 bg-red-600"></div>
+	<div class="js-ajax-search-spinner animate-spin h-5 w-5 mr-3 absolute top-3 -right-8"></div>
 </form>
