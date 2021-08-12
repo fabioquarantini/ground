9d6a7db28e0b21848ff66e1fadc35cbe94f5e575
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-08-12 09:54:12 +0200

    add search & modify menu.js

--- js/components/menu.js
+++ js/components/menu.js
@@ -3,38 +3,14 @@ import * as deepmerge from 'deepmerge';
 export default class Menu {
 	/**
 	 * @param {string} element - Selector
-	 * @param {Object} options - User options
 	 */
-	constructor(element, options) {
+	constructor(element) {
 		this.element = element || '.navigation__item.has-children';
 		this.defaults = {
-			triggers: this.element
+			triggers: this.element,
+			level: 0
 		};
 
-		this.options = options ? deepmerge(this.defaults, options) : this.defaults;
-
-		window.addEventListener('DOMContentLoaded', () => {
-			this.init();
-			this.initEvents(this.options.triggers);
-		});
-
-		window.addEventListener('resize', () => {
-			this.DOM.menuContainer.style.cssText += 'transform: none';
-			this.DOM.menuAllProducts.style.cssText += 'transform: none';
-
-			this.DOM.html.classList.remove('is-navigation-open');
-			this.DOM.html.classList.remove('is-sub-navigation-open');
-			this.DOM.html.classList.remove('is-all-products-open');
-
-			this.init();
-			this.initEvents(this.options.triggers);
-		});
-	}
-
-	/**
-	 * Init
-	 */
-	init() {
 		this.DOM = {
 			element: document.querySelectorAll(this.element),
 			html: document.querySelector('html'),
@@ -47,59 +23,117 @@ export default class Menu {
 			buttonAllProducts: document.querySelector('.js-button-all-products'),
 			menuAllProducts: document.querySelector('.js-navigation-all-products')
 		};
+
+		window.addEventListener('DOMContentLoaded', () => {
+			this.init(this.defaults.triggers, this.defaults.level);
+		});
+
+		window.addEventListener('resize', () => {
+			this.DOM.menuContainer.style.cssText += 'transform: none';
+			this.DOM.menuAllProducts.style.cssText += 'transform: none';
+
+			this.DOM.html.classList.remove('is-navigation-open');
+			this.DOM.html.classList.remove('is-sub-navigation-open');
+			this.DOM.html.classList.remove('is-all-products-open');
+
+			this.init(this.defaults.triggers, this.defaults.level);
+		});
 	}
 
 	/**
-	 * Initialize events
+	 * Init
 	 * @param {string} triggers - Selectors
 	 */
-	initEvents(triggers) {
-		let level = 0;
+	init(triggers, levelMenu) {
+		console.log('init');
+		let level = levelMenu;
 		let translation = 0;
 
-		[...document.querySelectorAll(triggers)].forEach((item) => {
-			let multiLevelMenu = (whichMenu) => {
-				item.addEventListener('click', (t) => {
-					var subMenu = null;
-					var subMenuImage = null;
-					t.target.parentNode.childNodes.forEach((sub) => {
-						sub.classList && sub.classList.contains('navigation__sub-menu') ? (subMenu = sub) : null;
-						sub.classList && sub.classList.contains('navigation__image') ? (subMenuImage = sub) : null;
+		let multiLevelMenu = (item, whichMenu) => {
+			item.addEventListener('click', (t, i) => {
+				console.log('click' + i);
+				var subMenu = null;
+				var subMenuImage = null;
+				t.target.parentNode.childNodes.forEach((sub) => {
+					sub.classList && sub.classList.contains('navigation__sub-menu') ? (subMenu = sub) : null;
+					sub.classList && sub.classList.contains('navigation__image') ? (subMenuImage = sub) : null;
+				});
+
+				if (subMenu) {
+					t.preventDefault();
+					t.stopPropagation();
+
+					t.target.parentElement.classList.add('level' + level);
+					this.DOM.html.classList.add('is-sub-navigation-open');
+
+					subMenu.classList.add('is-active');
+					subMenuImage && subMenuImage.classList.add('is-active');
+
+					level++;
+					translation = -100 * level;
+
+					if (whichMenu == this.DOM.menuContainer) {
+						this.DOM.menuContainer.style.cssText += 'transform: translateX(' + translation + 'vw);';
+					} else if (whichMenu == this.DOM.menuAllProducts) {
+						this.DOM.menuAllProducts.style.cssText += 'transform: translateX(' + translation + '%);';
+					}
+
+					this.DOM.menuBody.scrollTo({
+						top: 0,
+						left: 0,
+						behavior: 'smooth'
 					});
+				}
+			});
+		};
 
-					if (subMenu) {
-						t.preventDefault();
-						t.stopPropagation();
-
-						t.target.parentElement.classList.add('level' + level);
-						this.DOM.html.classList.add('is-sub-navigation-open');
-
-						subMenu.classList.add('is-active');
-						subMenuImage && subMenuImage.classList.add('is-active');
-
-						level++;
-						translation = -100 * level;
-
-						if (whichMenu == this.DOM.menuContainer) {
-							this.DOM.menuContainer.style.cssText += 'transform: translateX(' + translation + 'vw);';
-						} else if (whichMenu == this.DOM.menuAllProducts) {
-							this.DOM.menuAllProducts.style.cssText += 'transform: translateX(' + translation + '%);';
-						}
-
-						this.DOM.menuBody.scrollTo({
-							top: 0,
-							left: 0,
-							behavior: 'smooth'
-						});
+		let multiLevelBack = (whichMenu) => {
+			if (level > 0) {
+				level--;
+				[...document.querySelectorAll(triggers)].forEach((item) => {
+					if (item.classList.contains('level' + level)) {
+						item.classList.remove('level' + level);
+						item.childNodes.forEach((t) =>
+							t.classList && t.classList.contains('is-active') ? t.classList.remove('is-active') : null
+						);
 					}
 				});
-			};
+				let translation = -100 * level;
 
-			if (window.matchMedia('(max-width: 1024px)').matches) {
-				multiLevelMenu(this.DOM.menuContainer);
-			} else {
-				multiLevelMenu(this.DOM.menuAllProducts);
+				if (whichMenu == this.DOM.menuContainer) {
+					this.DOM.menuContainer.style.cssText += 'transform: translateX(' + translation + 'vw);';
+				} else if (whichMenu == this.DOM.menuAllProducts) {
+					this.DOM.menuAllProducts.style.cssText += 'transform: translateX(' + translation + '%);';
+				}
 
+				level == 0 ? this.DOM.html.classList.remove('is-sub-navigation-open') : null;
+			}
+		};
+
+		if (window.matchMedia('(max-width: 1024px)').matches) {
+			console.log('matchMedia mobile');
+			[...document.querySelectorAll(triggers)].forEach((item) => {
+				multiLevelMenu(item, this.DOM.menuContainer);
+			});
+
+			this.DOM.back.forEach((b) => {
+				b.addEventListener('click', () => {
+					multiLevelBack(this.DOM.menuContainer);
+				});
+			});
+		} else {
+			console.log('matchMedia desk');
+			[...document.querySelectorAll(triggers)].forEach((item) => {
+				multiLevelMenu(item, this.DOM.menuAllProducts);
+			});
+
+			this.DOM.back.forEach((b) => {
+				b.addEventListener('click', () => {
+					multiLevelBack(this.DOM.menuAllProducts);
+				});
+			});
+
+			[...document.querySelectorAll(triggers)].forEach((item) => {
 				let timerHandle = null;
 				item.addEventListener('mouseenter', () => {
 					clearTimeout(timerHandle);
@@ -116,73 +150,40 @@ export default class Menu {
 						}, 300);
 					}
 				});
-			}
+			});
+		}
+
+		this.DOM.navicon.addEventListener('click', () => {
+			this.DOM.html.classList.remove('is-sub-navigation-open');
+			// if (window.matchMedia('(min-width: 1024px)').matches) {
+			//  setTimeout(() => {
+			//      this.DOM.menuAllProducts.style.cssText += 'transform: none';
+			//      this.DOM.menuContainer.style.cssText += 'transform: none';
+			//  }, 350);
+			// }
+			//level = 0;
 		});
 
-		let multiLevelBack = (whichMenu) => {
-			if (level > 0) {
-				level--;
+		this.DOM.closeAllProducts.addEventListener('click', () => {
+			this.DOM.html.classList.remove('is-sub-navigation-open');
+			// if (window.matchMedia('(min-width: 1024px)').matches) {
+			//  setTimeout(() => {
+			//      this.DOM.menuAllProducts.style.cssText += 'transform: none';
+			//      this.DOM.menuContainer.style.cssText += 'transform: none';
+			//  }, 350);
+			// }
+			//level = 0;
+		});
 
-				[...document.querySelectorAll(triggers)].forEach((item) => {
-					if (item.classList.contains('level' + level)) {
-						item.classList.remove('level' + level);
-						item.childNodes.forEach((t) =>
-							t.classList && t.classList.contains('is-active') ? t.classList.remove('is-active') : null
-						);
-					}
-				});
-
-				let translation = -100 * level;
-
-				if (whichMenu == this.DOM.menuContainer) {
-					this.DOM.menuContainer.style.cssText += 'transform: translateX(' + translation + 'vw);';
-				} else if (whichMenu == this.DOM.menuAllProducts) {
-					this.DOM.menuAllProducts.style.cssText += 'transform: translateX(' + translation + '%);';
-				}
-
-				level == 0 ? this.DOM.html.classList.remove('is-sub-navigation-open') : null;
-			}
-		};
-
-		this.DOM.back &&
-			this.DOM.back.forEach((b) => {
-				b.addEventListener('click', () => {
-					if (window.matchMedia('(max-width: 1024px)').matches) {
-						multiLevelBack(this.DOM.menuContainer);
-					} else {
-						multiLevelBack(this.DOM.menuAllProducts);
-					}
-				});
-			});
-
-		this.DOM.navicon &&
-			this.DOM.navicon.addEventListener('click', () => {
-				this.DOM.html.classList.remove('is-sub-navigation-open');
-				setTimeout(() => {
-					this.DOM.menuAllProducts.style.cssText += 'transform: none';
-					this.DOM.menuContainer.style.cssText += 'transform: none';
-				}, 350);
-				level = 0;
-			});
-
-		this.DOM.closeAllProducts &&
-			this.DOM.closeAllProducts.addEventListener('click', () => {
-				this.DOM.html.classList.remove('is-sub-navigation-open');
-				setTimeout(() => {
-					this.DOM.menuAllProducts.style.cssText += 'transform: none';
-					this.DOM.menuContainer.style.cssText += 'transform: none';
-				}, 350);
-				level = 0;
-			});
-
-		this.DOM.closePanelAllProducts &&
-			this.DOM.closePanelAllProducts.addEventListener('click', () => {
-				this.DOM.html.classList.remove('is-sub-navigation-open');
-				setTimeout(() => {
-					this.DOM.menuAllProducts.style.cssText += 'transform: none';
-					this.DOM.menuContainer.style.cssText += 'transform: none';
-				}, 350);
-				level = 0;
-			});
+		this.DOM.closePanelAllProducts.addEventListener('click', () => {
+			this.DOM.html.classList.remove('is-sub-navigation-open');
+			// if (window.matchMedia('(min-width: 1024px)').matches) {
+			//  setTimeout(() => {
+			//      this.DOM.menuAllProducts.style.cssText += 'transform: none';
+			//      this.DOM.menuContainer.style.cssText += 'transform: none';
+			//  }, 350);
+			// }
+			//level = 0;
+		});
 	}
 }
--- partials/search-form.php
+++ partials/search-form.php
@@ -1,15 +1,13 @@
 <div class="search">
 	<div class="search__bg js-toggle" data-toggle-target=".search html" data-toggle-class-name="is-search-open"></div>
 	<div class="search__content">
-		<form class="form form--search" id="js-ajax-search" method="get" action="<?php echo home_url( '/' ); ?>">
-			<label class="form__label" for="s"><?php _e('What are you looking for?', 'ground') ?></label>
-			<input class="form__input" type="text" id="js-ajax-search-input" placeholder="<?php _e('Search products', 'ground') ?>" value="<?php the_search_query(); ?>" name="s" id="s" />
-			<button class="form__button" type="submit"><?php ground_icon( 'search', 'form__icon' ); ?></button>
-			<div class="search__spinner spinner js-ajax-search-spinner"></div>
-		</form> <!-- End .form -->
+		<h4 class="text-lg"><?php _e( 'What are you looking for?', 'ground' ); ?></h4>
+		<?php get_template_part( 'partials/search', 'form-input' ); ?>
+
 		<a class="search__close js-toggle js-magnet js-cursor-hover" href="#" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
 			<?php ground_icon( 'close' ); ?>
 		</a>
-		<div class="search__result" id="js-ajax-search-result"></div>
+
+		<?php get_template_part( 'partials/search', 'result' ); ?>
 	</div>
 </div>
