fee24396314a4ee729280b0d2067776256ba8144
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-13 10:50:55 +0200

    Add ground_add_sidebar_woocommerce_advanced + overlay-panel

--- inc/woocommerce/product-loop.php
+++ inc/woocommerce/product-loop.php
@@ -130,3 +130,34 @@ function ground_archive_filters_buttons() {
 }
 
 add_action( 'woocommerce_before_shop_loop', 'ground_archive_filters_buttons', 10 );
+
+
+
+
+
+/**
+ * WooCommerce, Add sidebar woocommerce advanced
+ */
+function ground_add_sidebar_woocommerce_advanced() {
+	?>
+
+	<div class="button js-toggle" data-toggle-target="html" data-toggle-class-name="is-overlay-panel-open">Open sidebar</div>
+
+	<!-- overlay-panel--from-left -->
+	<div class="overlay-panel">
+		<div class="overlay-panel__mask js-toggle" data-toggle-target="html" data-toggle-class-name="is-overlay-panel-open"></div>
+		<div class="overlay-panel__body">
+			<div class="overlay-panel__close js-toggle" data-toggle-target="html" data-toggle-class-name="is-overlay-panel-open"><?php ground_icon( 'close' ); ?></div>
+			<div class="overlay-panel__content">
+				<h1>Lorem Isum</h1>
+				<p class="text-4xl">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend eget odio aliquam varius. Curabitur ut porta ligula, ac placerat odio. Vestibulum facilisis ligula a purus imperdiet rutrum. Curabitur non diam tristique sem pharetra tincidunt eu a augue. Duis dictum nulla in ante consequat, sed maximus libero sodales. Cras dapibus nunc id sagittis rhoncus. Duis volutpat est justo, vel ultrices dui varius eu. Nullam gravida vehicula blandit. Sed varius felis id tortor tincidunt volutpat. Nam interdum at metus sed hendrerit. Praesent pretium vitae tellus eget varius. Integer consequat tristique nulla, sed ultricies tortor bibendum vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sed tincidunt justo. Morbi vel purus ut purus pulvinar hendrerit euismod at est.
+					Nunc in risus vel massa convallis sodales et quis diam. Nullam finibus dui ut augue sollicitudin malesuada. Maecenas aliquam, libero ut vestibulum tempor, lacus erat faucibus metus, ut egestas sapien lorem nec ante. Nunc cursus rutrum libero et cursus. Etiam felis ligula, iaculis nec dictum at, semper a ipsum. Donec dolor velit, finibus ut laoreet eu, laoreet sed sem. Donec commodo est sit amet gravida molestie. Nam tortor nulla, commodo a ornare eu, aliquam sit amet dolor. Donec non libero in lorem aliquam iaculis eu ac augue. Praesent dictum quam et nibh dictum rhoncus. Morbi cursus lacus nec euismod laoreet. Donec in dui a elit varius tincidunt vitae quis lacus.</p>
+			</div>
+		</div>
+	</div>
+
+	<?php
+}
+
+add_action( 'woocommerce_sidebar', 'ground_add_sidebar_woocommerce_advanced', 10 );
--- inc/woocommerce/sidebar.php
+++ inc/woocommerce/sidebar.php
@@ -24,8 +24,18 @@ add_action( 'init', 'ground_woocommerce_disable_woocommerce_sidebar' );
 function ground_woocommerce_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => __( 'Shop sidebar', 'ground' ),
+			'name'          => __( 'Shop Sidebar', 'ground' ),
 			'id'            => 'sidebar-woocommerce',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h6 class="woocommerce-widget-layered-nav__title">',
+			'after_title'   => '</h6>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => __( 'Shop Sidebar Advanced', 'ground' ),
+			'id'            => 'sidebar-woocommerce-advanced',
 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</div>',
 			'before_title'  => '<h6 class="woocommerce-widget-layered-nav__title">',
