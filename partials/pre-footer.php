c9f638320cb68634ab39649485c5cc14517e7574
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-08-17 12:30:34 +0200

    fix multiselect logo shop

--- partials/pre-footer.php
+++ partials/pre-footer.php
@@ -1,31 +1,21 @@
 <?php
-$repeater = GROUND_FOOTER_REPEATER
+$repeater = GROUND_SHOP_PAYMENT;
+
 ?>
 
  <div class="container my-12 lg:my-24">
 
 	<div class="text-center mb-24 grid grid-cols-12 gap-6 lg:items-center">
-			<?php if ( $repeater ) : ?>
+			 <?php if ( $repeater ) : ?>
 			<div class="col-span-full lg:col-span-2">
 				<div class="grid grid-cols-4 gap-6 lg:grid-cols-2">
 					<?php
 
 					foreach ( $repeater as $row ) :
-						$select   = $row['select'];
-						$checkbox = $row['checkbox'];
-						$svg      = $row['svg'];
 						?>
-
-						<?php if ( ! $checkbox ) { ?>
-							<figure class="col-span-1 aspect-w-1 aspect-h-1 rounded-full bg-gray-100 flex justify-center mb-4">
-								<img class="object-contain p-6" src="<?php echo GROUND_TEMPLATE_URL . '/img/getway-icons/' . $select . '.svg'; ?>"/>
-							</figure>
-						<?php } else { ?>
-							<div class="col-span-1 aspect-w-1 aspect-h-1 rounded-full bg-gray-100 flex justify-center mb-4">
-								<div class="flex items-center justify-center"><?php echo $svg; ?></div>
-							</div>
-						<?php } ?>
-
+						<figure class="col-span-1 aspect-w-1 aspect-h-1 rounded-full bg-gray-100 flex justify-center mb-4">
+							<img class="object-contain p-6" src="<?php echo GROUND_TEMPLATE_URL . '/img/getway-icons/' . $row . '.svg'; ?>"/>
+						</figure>
 					<?php endforeach; ?>
 				</div>
 			</div>
