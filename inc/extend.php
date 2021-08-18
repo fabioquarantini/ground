7c98423b02add288355130cf0160cbb6903caac3
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-08-18 09:31:12 +0200

    Add load_template_part

--- inc/extend.php
+++ inc/extend.php
@@ -645,6 +645,18 @@ function get_the_sidebar( $sidebar ) {
 	return ob_get_clean();
 }
 
+/**
+ * Get template part without echo
+ */
+
+function load_template_part( $template_name, $part_name = null ) {
+	ob_start();
+	get_template_part( $template_name, $part_name );
+	$var = ob_get_contents();
+	ob_end_clean();
+	return $var;
+}
+
 
 /**
  * Ovarlay Panel creator
