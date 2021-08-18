6616e6359ac57249b22003b1f8cae11c0721e856
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-08-18 18:04:31 +0200

    fix header simple

--- partials/content-header.php
+++ partials/content-header.php
@@ -30,29 +30,30 @@
 					<a class="border border-dashed border-primary p-2" target="_blank" href="<?php echo admin_url( 'admin.php?page=theme-general-settings', 'http' ); ?>">Upload your logo</a>
 				<?php } ?>
 			</div>
+			<div class="flex justify-end items-center">
+				<button class="js-toggle js-cursor-hover hidden mr-12 lg:block" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
+					<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
+				</button>
 
-			<!-- <button class="js-toggle js-cursor-hover hidden mr-12 lg:block" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
-				<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
-			</button> -->
+				<?php
+					$count = WC()->cart->get_cart_contents_count();
+					$class = '';
 
-			<?php
-				$count = WC()->cart->get_cart_contents_count();
-				$class = '';
+				if ( $count === 0 ) {
+					$class = 'is-empty ';
+				} else {
+					$class = 'is-full js-toggle';
+				}
+				?>
 
-			if ( $count === 0 ) {
-				$class = 'is-empty ';
-			} else {
-				$class = 'is-full js-toggle';
-			}
-			?>
-
-			<div class="header__cart relative lg:hidden">
-				<a class="flex justify-start items-center" href="<?php echo wc_get_page_permalink( 'cart' ); ?>">
-					<?php ground_icon( 'shopping-cart', 'minicart__icon text-black dark:text-white' ); ?>
-					<div class="absolute -right-4 -top-2 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-white font-bold">
-						<?php echo WC()->cart->get_cart_contents_count(); ?>
-					</div>
-				</a>
+				<div class="header__cart relative lg:hidden">
+					<a class="flex justify-start items-center" href="<?php echo wc_get_page_permalink( 'cart' ); ?>">
+						<?php ground_icon( 'shopping-cart', 'minicart__icon text-black dark:text-white' ); ?>
+						<div class="absolute -right-4 -top-2 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-white font-bold">
+							<?php echo WC()->cart->get_cart_contents_count(); ?>
+						</div>
+					</a>
+				</div>
 			</div>
 
 		</div>
@@ -66,7 +67,12 @@
 					<?php echo GROUND_HEADER_TEXT; ?>
 				</div>
 
-				<div class="flex items-center justify-end space-x-5">
+				<div class="flex items-center justify-end space-x-6">
+					<?php if ( has_nav_menu( 'header-secondary' ) ) : ?>
+					<div class="lg:border-r lg:border-gray-200 lg:pr-6">
+						<?php get_template_part( 'partials/navigation', 'header-secondary' ); ?>
+					</div>
+					<?php endif; ?>
 					<ul class="flex flex-row-reverse">
 						<?php if ( function_exists( 'icl_object_id' ) ) : ?>
 						<li class="js-toggle text-lg lg:text-base pl-4" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'globe-alt', 'icon--filled w-6 rounded-full text-white bg-black p-1 dark:text-white dark:bg-white' ); ?></span><?php _e( 'Language', 'ground' ); ?><span><?php ground_icon( 'chevron-down', 'text-black' ); ?></span></a></li>
@@ -79,9 +85,9 @@
 						<?php endif; ?>
 					</ul>
 					<?php if ( ! function_exists( 'is_woocommerce_activated' ) ) : ?>
-					<ul class=" relative z-0 border-l border-gray-200 pl-6 lg:flex lg:items-center lg:space-x-5 lg:justify-end lg:m-0">
-						<li class="text-lg lg:text-base"><a class="inline-block py-4 lg:py-auto" href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php _e( 'Account', 'ground' ); ?><span class="hidden ml-2 lg:inline-block"><?php ground_icon( 'user', 'icon--filled text-black dark:text-white' ); ?></span></a></li>
-						<li class="hidden minicart-wrapper lg:inline-block"><?php get_template_part( 'partials/woocommerce/shopping-cart' ); ?> </li>
+					<ul class=" relative z-0 lg:flex lg:items-center lg:space-x-5 lg:justify-end lg:m-0">
+						<li class="text-lg lg:border-l lg:border-gray-200 lg:pl-6 lg:text-base"><a class="inline-block lg:py-auto" href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php _e( 'Account', 'ground' ); ?><span class="hidden ml-2 lg:inline-block"><?php ground_icon( 'user', 'icon--filled text-black dark:text-white' ); ?></span></a></li>
+						<li class="hidden minicart-wrapper py-4 lg:inline-block"><?php get_template_part( 'partials/woocommerce/shopping-cart' ); ?> </li>
 					</ul>
 					<?php endif; ?>
 				</div>
@@ -90,52 +96,42 @@
 			<div class="flex flex-col-reverse lg:block">
 
 				<div class="container lg:relative lg:flex lg:justify-between lg:items-center lg:h-16">
-					<div class="relative z-1 lg:flex lg:items-center lg:justify-start lg:space-x-3">
-						<div class="hidden lg:inline-block mr-8">
-							<?php if ( GROUND_LOGO_URL_PRIMARY || GROUND_LOGO_SOURCE_PRIMARY ) { ?>
-							<a class="js-cursor-hide" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
-								<?php
-								if ( GROUND_LOGO_TYPE_PRIMARY && GROUND_LOGO_SOURCE_PRIMARY ) {
-									echo GROUND_LOGO_SOURCE_PRIMARY;
-								} elseif ( GROUND_LOGO_URL_PRIMARY ) {
-									?>
-									<img class="w-20 h-auto" src="<?php echo esc_url( GROUND_LOGO_URL_PRIMARY['sizes']['medium'] ); ?>" alt="<?php echo esc_attr( GROUND_LOGO_URL_PRIMARY['alt'] ); ?>" />
-								<?php } ?>
-							</a> <!-- End logo -->
-							<?php } else { ?>
-								<a class="border border-dashed border-primary p-2" target="_blank" href="<?php echo admin_url( 'admin.php?page=theme-general-settings', 'http' ); ?>">Upload your logo</a>
+
+					<div class="hidden lg:inline-block mr-8">
+						<?php if ( GROUND_LOGO_URL_PRIMARY || GROUND_LOGO_SOURCE_PRIMARY ) { ?>
+						<a class="js-cursor-hide" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
+							<?php
+							if ( GROUND_LOGO_TYPE_PRIMARY && GROUND_LOGO_SOURCE_PRIMARY ) {
+								echo GROUND_LOGO_SOURCE_PRIMARY;
+							} elseif ( GROUND_LOGO_URL_PRIMARY ) {
+								?>
+								<img class="w-20 h-auto" src="<?php echo esc_url( GROUND_LOGO_URL_PRIMARY['sizes']['medium'] ); ?>" alt="<?php echo esc_attr( GROUND_LOGO_URL_PRIMARY['alt'] ); ?>" />
 							<?php } ?>
-						</div>
-
-						<?php get_template_part( 'partials/navigation', 'header-secondary' ); ?>
-
-
-						 <button class="js-toggle  mr-12 lg:block" data-toggle-target="#js-search-form html" data-toggle-class-name="is-search-open">
-							<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
-						</button>
+						</a> <!-- End logo -->
+						<?php } else { ?>
+							<a class="border border-dashed border-primary p-2" target="_blank" href="<?php echo admin_url( 'admin.php?page=theme-general-settings', 'http' ); ?>">Upload your logo</a>
+						<?php } ?>
 					</div>
 
-
-
-					<ul class="block lg:hidden">
-						<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'globe-alt', 'icon--filled w-6 rounded-full text-white bg-black p-1 dark:text-white dark:bg-white' ); ?></span><?php _e( 'Language', 'ground' ); ?></a></li>
-						<?php if ( GROUND_COMPANY_PHONE ) : ?>
-							<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'phone', 'icon--filled w-6 rounded-full text-white bg-purple-600 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_PHONE; ?></a></li>
-						<?php endif; ?>
-						<?php if ( GROUND_COMPANY_WHATSAPP ) : ?>
-							<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'whatsapp', 'icon--filled w-6 rounded-full text-white bg-green-400 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_WHATSAPP; ?></a></li>
-						<?php endif; ?>
-					</ul>
-				</div>
-
-
-
-				<div class="container lg:relative lg:flex lg:items-center lg:justify-between lg:h-16">
+					<div class="lg:flex lg:justify-end lg:items-center">
 
 						<?php get_template_part( 'partials/navigation', 'header-primary' ); ?>
 
-				</div>
+						<button class="hidden js-toggle lg:block" data-toggle-target="#js-search-form html" data-toggle-class-name="is-search-open">
+							<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
+						</button>
 
+						<ul class="block lg:hidden">
+							<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'globe-alt', 'icon--filled w-6 rounded-full text-white bg-black p-1 dark:text-white dark:bg-white' ); ?></span><?php _e( 'Language', 'ground' ); ?></a></li>
+							<?php if ( GROUND_COMPANY_PHONE ) : ?>
+								<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'phone', 'icon--filled w-6 rounded-full text-white bg-purple-600 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_PHONE; ?></a></li>
+							<?php endif; ?>
+							<?php if ( GROUND_COMPANY_WHATSAPP ) : ?>
+								<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'whatsapp', 'icon--filled w-6 rounded-full text-white bg-green-400 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_WHATSAPP; ?></a></li>
+							<?php endif; ?>
+						</ul>
+					</div>
+				</div>
 			</div>
 
 		</div>
