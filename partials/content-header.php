8711dd284b7ce907004fe9e81976663633ddeaaf
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-08-18 15:32:23 +0200

    modify extend

--- partials/content-header.php
+++ partials/content-header.php
@@ -1,11 +1,14 @@
-<div class="container bg-black text-center text-white py-2 xl:hidden">
+
+<div class="message-alert message-alert--primary container <?php echo GROUND_HEADER_TEXT ? 'bg-black py-2' : ''; ?> text-center text-white  xl:hidden">
 	<?php echo GROUND_HEADER_TEXT; ?>
 </div>
 
-<header class="header header-simple w-full z-30 bg-white dark:bg-black">
 
-	<div class="sticky top-0 bg-white dark:bg-black h-16 w-full z-30 lg:hidden">
-		<a class="js-back absolute top-4 left-4 header__back cursor-pointer"> <span> <?php ground_icon( 'chevron-left', 'text-black dark:text-white' ); ?> </span> <?php _e( 'Indietro', 'ground' ); ?> </a>
+
+<header class="header header-mega-menu w-full z-30 bg-white dark:bg-black">
+
+	<div class="bg-white dark:bg-black h-16 w-full z-30 lg:hidden">
+		<a class="js-back absolute mt-5 left-4 header__back cursor-pointer"> <span> <?php ground_icon( 'chevron-left', 'text-black dark:text-white' ); ?> </span> <?php _e( 'Indietro', 'ground' ); ?> </a>
 
 		<div class="header__navicon container flex items-center justify-between py-2 bg-white dark:bg-black">
 			<svg class="js-toggle js-navicon navicon h-12 w-12 -ml-4 cursor-pointer block" data-toggle-target="html" data-toggle-class-name="is-navigation-open" viewBox="0 0 100 100" width="80">
@@ -30,6 +33,9 @@
 				<?php } ?>
 			</div>
 
+			<button class="js-toggle js-cursor-hover hidden mr-12 lg:block" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
+				<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
+			</button> <!-- End .header__search -->
 
 			<?php
 				$count = WC()->cart->get_cart_contents_count();
@@ -52,25 +58,23 @@
 			</div>
 
 		</div>
+
 	</div>
 
-	<div class="js-menu-body header__body grid auto-rows-min fixed left-0 h-full w-full z-40 bg-white dark:bg-black overflow-y-auto lg:pt-0 lg:mt-0
- lg:relative lg:top-auto lg:left-auto lg:bottom-auto lg:right-auto lg:bg-transparent lg:overflow-y-visible">
+	<div class="js-menu-body header__body grid auto-rows-min fixed left-0 h-full w-full z-40 bg-white dark:bg-black overflow-y-auto lg:pt-0 lg:mt-0 lg:relative lg:top-auto lg:left-auto lg:bottom-auto lg:right-auto lg:bg-transparent lg:overflow-y-visible">
 		<div class="js-menu-container header__container relative">
 			<div class="container hidden lg:relative lg:flex lg:justify-between lg:items-center lg:h-16">
 				<div class="hidden xl:flex">
 					<?php echo GROUND_HEADER_TEXT; ?>
 				</div>
-				<ul class="block m-0 space-x-0 lg:space-x-5">
-					<li class="js-toggle cursor-pointer text-lg lg:text-base lg:py-0" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a><span class="mr-2"><?php ground_icon( 'globe-alt', 'icon--filled text-black dark:text-white' ); ?></span><?php _e( 'Language', 'ground' ); ?></a></li>
-				</ul>
+
 			</div>
 
 			<div class="flex flex-col-reverse lg:block">
 
 				<div class="container lg:relative lg:flex lg:justify-between lg:items-center lg:h-16">
-					<div class="lg:flex lg:items-center lg:justify-start lg:space-x-3">
-						<div class="hidden lg:inline-block">
+					<div class="relative z-1 lg:flex lg:items-center lg:justify-start lg:space-x-3">
+						<div class="hidden lg:inline-block mr-8">
 							<?php if ( GROUND_LOGO_URL_PRIMARY || GROUND_LOGO_SOURCE_PRIMARY ) { ?>
 							<a class="js-cursor-hide" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
 								<?php
@@ -85,28 +89,56 @@
 								<a class="border border-dashed border-primary p-2" target="_blank" href="<?php echo admin_url( 'admin.php?page=theme-general-settings', 'http' ); ?>">Upload your logo</a>
 							<?php } ?>
 						</div>
-						<?php get_template_part( 'partials/navigation', 'secondary' ); ?>
 
-
-						<button class="js-toggle js-cursor-hover hidden mr-12 lg:block" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
-							<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
-						</button> <!-- End .header__search -->
+						<?php get_template_part( 'partials/navigation', 'header-secondary' ); ?>
+						<div id="js-search-desktop" class="relative">
+							<?php get_template_part( 'partials/search', 'form-input' ); ?>
+						</div>
 					</div>
 
+					<ul class="relative z-0 border-b border-gray-200 lg:border-none lg:flex lg:items-center lg:space-x-5 lg:justify-end lg:m-0">
+						<li class="text-lg lg:text-base"><a class="inline-block py-4 lg:py-auto" href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php _e( 'Account', 'ground' ); ?><span class="hidden ml-2 lg:inline-block"><?php ground_icon( 'user', 'icon--filled text-black dark:text-white' ); ?></span></a></li>
+						<li class="hidden minicart-wrapper lg:inline-block"><?php get_template_part( 'partials/woocommerce/shopping-cart' ); ?> </li>
+					</ul>
 
-					<ul class="flex items-center mt-6 justify-start space-x-5 lg:justify-end lg:m-0">
-						<li class="text-lg lg:text-base"><a class="navigation__link"href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php _e( 'Account', 'ground' ); ?><span class="ml-2"><?php ground_icon( 'user', 'icon--filled text-black dark:text-white' ); ?></span></a></li>
-						<li class="hidden minicart-wrapper lg:inline-block"><?php get_template_part( 'partials/woocommerce/minicart' ); ?> </li>
+					<ul class="block lg:hidden">
+						<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'globe-alt', 'icon--filled w-6 rounded-full text-white bg-black p-1 dark:text-white dark:bg-white' ); ?></span><?php _e( 'Language', 'ground' ); ?></a></li>
+						<?php if ( GROUND_COMPANY_PHONE ) : ?>
+							<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'phone', 'icon--filled w-6 rounded-full text-white bg-purple-600 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_PHONE; ?></a></li>
+						<?php endif; ?>
+						<?php if ( GROUND_COMPANY_WHATSAPP ) : ?>
+							<li class="js-toggle text-lg lg:text-base py-3" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'whatsapp', 'icon--filled w-6 rounded-full text-white bg-green-400 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_WHATSAPP; ?></a></li>
+						<?php endif; ?>
 					</ul>
 				</div>
 
 
+
 				<div class="container lg:relative lg:flex lg:items-center lg:justify-between lg:h-16">
-					  <?php get_template_part( 'partials/navigation', 'primary' ); ?>
+					<div class="<?php echo has_nav_menu( 'panel-primary' ) ? 'hidden lg:flex' : ''; ?>">
+						<?php get_template_part( 'partials/navigation', 'header-primary' ); ?>
+					</div>
+
+					<?php if ( has_nav_menu( 'panel-primary' ) ) { ?>
+						<div class="header__panel">
+							<a class="js-toggle hidden cursor-pointer lg:flex lg:justify-end lg:items-center lg:text-lg" data-toggle-target="#panel-primary html" data-toggle-class-name="is-overlay-panel-open">
+								<div class="mr-2 flex justify-end items-center"> <?php ground_icon( 'menu-left', 'icon--filled text-black dark:text-white' ); ?></div>
+								<div> <?php _e( 'All products', 'ground' ); ?></div>
+							</a>
+							<div class="hidden lg:flex">
+								<?php get_template_part( 'partials/panel', 'primary' ); ?>
+							</div>
+							<div class="lg:hidden">
+								<?php get_template_part( 'partials/navigation', 'panel-primary' ); ?>
+							</div>
+						</div>
+					<?php } ?>
 				</div>
 
 			</div>
 
 		</div>
 	</div>
+
 </header> <!-- End header -->
+
