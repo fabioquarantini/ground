d018b8656c5507063688018abdc8569f2c68e3ed
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-08-18 15:16:38 +0200

    revert  ground_overlay_panel

--- partials/woocommerce/minicart.php
+++ partials/woocommerce/minicart.php
@@ -1,13 +1,17 @@
 <?php if ( class_exists( 'WooCommerce' ) ) : ?>
 
 	<div class="minicart">
-		<?php
-		echo ground_overlay_panel(
-			'minicart',
-			get_the_widget( 'WC_Widget_Cart', 'title=Prodotti nel carrello&hide_if_empty=1' ),
-			'right'
-		);
-		?>
+		<div class="overlay-panel" id="minicart">
+			<div class="overlay-panel__mask js-toggle" data-toggle-target="#minicart html" data-toggle-class-name="is-overlay-panel-open"></div>
+			<div class="overlay-panel__body">
+				<div class="overlay-panel__close js-toggle" data-toggle-target="#minicart html" data-toggle-class-name="is-overlay-panel-open">
+					<?php ground_icon( 'close' ); ?>
+				</div>
+				<div class="overlay-panel__content">
+					<?php the_widget( 'WC_Widget_Cart', 'title=Prodotti nel carrello&hide_if_empty=1' ); ?>
+				</div>
+			</div>
+		</div>
 	</div>
 
 <?php endif; ?>
