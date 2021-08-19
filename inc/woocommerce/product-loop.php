ea528474e5751340e37e9a35a87e20f2e93fe741
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-08-19 18:01:09 +0200

    Add Display Discount Percentage @ Loop Pages - WooCommerce

--- inc/woocommerce/product-loop.php
+++ inc/woocommerce/product-loop.php
@@ -208,3 +208,40 @@ function ground_add_on_hover_shop_loop_image() {
 }
 
 add_action( 'woocommerce_before_shop_loop_item_title', 'ground_add_on_hover_shop_loop_image' );
+
+
+
+/**
+ * Display Discount Percentage @ Loop Pages - WooCommerce
+ */
+function ground_show_sale_percentage_loop() {
+	global $product;
+	if ( ! $product->is_on_sale() ) {
+		return;
+	}
+	$max_percentage = true;
+	if ( $product->is_type( 'simple' ) ) {
+		$max_percentage = ( ( $product->get_regular_price() - $product->get_sale_price() ) / $product->get_regular_price() ) * 100;
+	} elseif ( $product->is_type( 'variable' ) ) {
+		$max_percentage = 0;
+		foreach ( $product->get_children() as $child_id ) {
+			$variation  = wc_get_product( $child_id );
+			$price      = $variation->get_regular_price();
+			$sale       = $variation->get_sale_price();
+			$percentage = true;
+			if ( $price != 0 && ! empty( $sale ) ) {
+				$percentage = ( $price - $sale ) / $price * 100;
+			}
+			if ( $percentage > $max_percentage ) {
+				$max_percentage = $percentage;
+			}
+		}
+	}
+	if ( $max_percentage > 0 ) {
+		?>
+		<div class='onsale'> <?php _e( 'Sale!', 'woocommerce' ); ?> -<?php echo round( $max_percentage ); ?>%</div>
+		<?php
+	}
+}
+
+add_action( 'woocommerce_sale_flash', 'ground_show_sale_percentage_loop', 25 );
