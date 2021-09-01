436bbc4acf66261fb95c321ddafbc7d2622d4ebc
Fabio Quarantini <wlospam@gmail.com>
2021-09-01 12:38:06 +0200

    Update npm scripts

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,15 @@
 module.exports = (ctx) => ({
 	map: ctx.options.map,
-	syntax: 'postcss-scss',
 	plugins: {
 		'postcss-import': {},
+		'tailwindcss/nesting': {},
 		tailwindcss: {},
 		'postcss-for': {},
-		'postcss-calc': {
-			mediaQueries: true
-		},
+		// 'postcss-calc': {
+		// 	mediaQueries: true
+		// },
 		'postcss-simple-vars': {},
-		'postcss-nested': {},
-		'postcss-100vh-fix': ctx.env === 'production' ? {} : false,
+		'postcss-100vh-fix': true,
 		autoprefixer: ctx.env === 'production' ? {} : false,
 		cssnano: ctx.env === 'production' ? {} : false
 	}
