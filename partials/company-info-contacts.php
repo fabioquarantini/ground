f7b00edf9761fc391f37a98a281bf6f19e9f2962
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-09-01 18:14:17 +0200

    change info contancts

--- partials/company-info-contacts.php
+++ partials/company-info-contacts.php
@@ -4,10 +4,10 @@
 		<li class="js-toggle py-3 text-lg lg:pl-4 lg:text-base lg:py-0" data-toggle-target="#modal-languages" data-toggle-class-name="hidden"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'globe-alt', 'icon--filled w-6 rounded-full text-white bg-black p-1 dark:text-white dark:bg-white' ); ?></span><?php _e( 'Language', 'ground' ); ?><span><?php ground_icon( 'chevron-down', 'text-black' ); ?></span></a></li>
 	<?php endif; ?>
 	<?php if ( GROUND_COMPANY_PHONE ) : ?>
-		<li class="js-toggle py-3 text-lg lg:pl-4 lg:text-base lg:py-0"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'phone', 'icon--filled w-6 rounded-full text-white bg-purple-600 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_PHONE; ?></a></li>
+		<li class="py-3 text-lg lg:pl-4 lg:text-base lg:py-0"><a class="cursor-pointer" href="tel:<?php echo GROUND_COMPANY_PHONE; ?>"><span class="mr-2"><?php ground_icon( 'phone', 'icon--filled w-6 rounded-full text-white bg-purple-600 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_PHONE; ?></a></li>
 	<?php endif; ?>
 	<?php if ( GROUND_COMPANY_WHATSAPP ) : ?>
-		<li class="js-toggle py-3 text-lg lg:text-base lg:py-0"><a class="cursor-pointer"><span class="mr-2"><?php ground_icon( 'whatsapp', 'icon--filled w-6 rounded-full text-white bg-green-400 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_WHATSAPP; ?></a></li>
+		<li class="py-3 text-lg lg:text-base lg:py-0"><a class="cursor-pointer" href="https://wa.me/<?php echo GROUND_COMPANY_WHATSAPP; ?>"><span class="mr-2"><?php ground_icon( 'whatsapp', 'icon--filled w-6 rounded-full text-white bg-green-400 p-1 dark:text-white' ); ?></span> <?php echo GROUND_COMPANY_WHATSAPP; ?></a></li>
 	<?php endif; ?>
 </ul>
 <?php endif; ?>
